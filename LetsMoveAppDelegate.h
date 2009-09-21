ed62b8bf06896ee4488337a68a2eb15a890fcc14
Andy Kim <andy@potionfactory.com>
2009-09-21 12:53:44 -0700

    Fixed issue 4: Compile without errors when targeting Mac OS X 10.5 SDK

--- LetsMoveAppDelegate.h
+++ LetsMoveAppDelegate.h
@@ -8,7 +8,11 @@
 
 #import <Cocoa/Cocoa.h>
 
+#if MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_5
+@interface LetsMoveAppDelegate : NSObject {
+#else
 @interface LetsMoveAppDelegate : NSObject <NSApplicationDelegate> {
+#endif
     NSWindow *window;
 }
 
