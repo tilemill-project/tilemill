02168c41fb4a65df8cfa991b7ebcc81b4f0fd378
Young Hahn <yfhahn@gmail.com>
2010-07-05 18:36:25 -0400

    Adding dummy click handlers to editor.js

--- tilemill/static/js/editor.js
+++ tilemill/static/js/editor.js
@@ -10,14 +10,24 @@ TileMill.addLayer = function(classes, id, status) {
   $('<li>')
     .append($('<input type="checkbox" />').attr('checked', status ? 'checked' : ''))
     .append(layerName)
-    .append($('<a href="#">Edit</a>'))
-    .append($('<a href="#">Inspect</a>'))
-    .appendTo($('ul.sidebar-content'));
+    .append($('<a class="layer-edit" href="#">Edit</a>'))
+    .append($('<a class="layer-inspect" href="#">Inspect</a>'))
+    .appendTo($('#layers ul.sidebar-content'));
 };
 
 $(function() {
   $(mml).find('Layer').each(function() {
     status = $(this).attr('status') == 'on';
     TileMill.addLayer($(this).attr('class').split(' '), $(this).attr('id'), !status || $(this).attr('status') == 'on');
+  });
+
+  $('a.layer-inspect').click(function() {
+    $('#layers').hide();
+    $('#inspector').show();
+  });
+
+  $('a.inspector-close').click(function() {
+    $('#layers').show();
+    $('#inspector').hide();
   });
 });
