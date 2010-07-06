02c392454111ffbca62fd1a6f938862172734500
Young Hahn <yfhahn@gmail.com>
2010-07-05 22:44:07 -0400

    Add basic map preview display

--- tilemill/static/js/editor.js
+++ tilemill/static/js/editor.js
@@ -21,6 +21,45 @@ TileMill.addLayer = function(classes, id, status) {
     .appendTo($('#layers ul.sidebar-content'));
 };
 
+TileMill.initMap = function(hosts, layername, type) {
+  // @TODO: have these options be passed by TileMill to point to TileLiteLive.
+  var hosts = hosts || [ "http://a.tile.mapbox.com/","http://b.tile.mapbox.com/", "http://c.tile.mapbox.com/", "http://d.tile.mapbox.com/" ],
+      layername = layername || 'world-light',
+      type = type || 'png';
+
+  if (!$('#map-preview').is('.inited')) {
+    $('#map-preview').addClass('inited');
+    var options = {
+      projection: new OpenLayers.Projection("EPSG:900913"),
+      displayProjection: new OpenLayers.Projection("EPSG:4326"),
+      units: "m",
+      numZoomLevels: 12,
+      maxResolution: 156543.0339,
+      maxExtent: new OpenLayers.Bounds(
+        -20037500,
+        -20037500,
+        20037500,
+        20037500
+      ),
+      controls: []
+    };
+    var map = new OpenLayers.Map('map-preview', options);
+    var layer = new OpenLayers.Layer.TMS("Preview", hosts, {layername: layername, type: type});
+    map.addLayers([ layer ]);
+
+    // Set the map's initial center point
+    map.setCenter(new OpenLayers.LonLat(0, 0), 2);
+
+    // Add control
+    var control = new OpenLayers.Control.Navigation({ 'zoomWheelEnabled': true });
+    map.addControl(control);
+    control.activate();
+  }
+  // Update map.
+  else {
+  }
+};
+
 $(function() {
   $(mml).find('Layer').reverse().each(function() {
     status = $(this).attr('status');
@@ -32,4 +71,6 @@ $(function() {
     $('#inspector').hide();
     return false;
   });
+
+  TileMill.initMap();
 });
