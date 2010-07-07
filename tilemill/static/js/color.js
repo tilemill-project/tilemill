71051768c41260b8b0ae3e78f6f7dda3df06fc0c
Young Hahn <yfhahn@gmail.com>
2010-07-07 15:17:52 -0400

    Fix reckless iteration.

--- tilemill/static/js/color.js
+++ tilemill/static/js/color.js
@@ -20,24 +20,27 @@ TileMill.colors.reload = function(data) {
   // Find all colors.
   var matches = data.match(/\#[A-Fa-f0-9]{3,6}/g),
   // Keep track of unique colors.
-    colors = [];
-  for (var i = 0; i < matches.length; i++) {
-    // Split up the color into an HSL triplet.
-    var color = TileMill.colors.farbtastic.RGBToHSL(TileMill.colors.farbtastic.unpack(matches[i]));
-    // Add the RGB to the color.
-    color.push(matches[i]);
-    var pass = false;
-    for (var key in colors) {
-      if (colors[key][3] == matches[i]) {
-        pass = true;
+  colors = [];
+
+  if (matches) {
+    for (var i = 0; i < matches.length; i++) {
+      // Split up the color into an HSL triplet.
+      var color = TileMill.colors.farbtastic.RGBToHSL(TileMill.colors.farbtastic.unpack(matches[i]));
+      // Add the RGB to the color.
+      color.push(matches[i]);
+      var pass = false;
+      for (var key in colors) {
+        if (colors[key][3] == matches[i]) {
+          pass = true;
+        }
+      }
+      if (!pass) {
+        colors.push(color);
       }
     }
-    if (!pass) {
-      colors.push(color);
-    }
+    // Sort the colors by lightness.
+    colors.sort(function(a, b) { return a[2] - b[2] });
   }
-  // Sort the colors by lightness.
-  colors.sort(function(a, b) { return a[2] - b[2] });
 
   var colors_div = $('div#colors div').empty();
   // Go through the colors and add them to the color palette. When one is
