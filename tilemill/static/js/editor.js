cfc94b6666a2f0140d1dacb7779b1c5c215f3db6
Young Hahn <yfhahn@gmail.com>
2010-07-09 11:55:36 -0400

    Revert "Merge branch 'unstable' of git@github.com:developmentseed/TileMill"
    This reverts commit 73eaefaa2511d3d4eecede8447e618dde3e2b7da.

--- tilemill/static/js/editor.js
+++ tilemill/static/js/editor.js
@@ -1,6 +1,35 @@
-TileMill.controller.edit = function() {
-  TileMill.show(TileMill.template('list', {
-    projects: TileMill.template('column', { name: 'Projects', data: ['asf', 'bar'] }),
-    visualizations: TileMill.template('column', { name: 'Visualizations', data: ['baz', 'asdf'] }),
-  }));
+var TileMill = TileMill || { settings:{}, page:0, uniq: (new Date().getTime()), customSrs: [], url: '', editor: {} };
+$.fn.reverse = [].reverse;
+
+TileMill.save = function() {
+  var mml = TileMill.mml.save(TileMill.mml.generate());
+
+  // Make sure latest edits to active tab's text have been recorded.
+  $('#tabs a.active input').val(TileMill.mirror.getCode());
+  $('#tabs a.tab').each(function() {
+    var url = $.url.setUrl($(this).data('tilemill')['src']);
+    TileMill.stylesheet.save(url.param('filename'), $('input', this).val());
+  });
+
+  TileMill.inspector.load();
+  TileMill.uniq = (new Date().getTime());
+  TileMill.map.reload();
 }
+
+$(function() {
+  $('div#header a.save').click(function() {
+    TileMill.save();
+    return false;
+  });
+
+  $('div#header a.info').click(function() {
+    $('#popup-info input#tilelive-url').val(TileMill.settings.tilelive.split(',')[0] + 'tile/' + TileMill.mml.url({ timestamp: false, encode: true }));
+    $('#popup-info input#project-mml-url').val(TileMill.mml.url({ timestamp: false, encode: false }));
+    TileMill.popup.show({content: $('#popup-info'), title: 'Info'});
+    return false;
+  });
+
+  for (var i in TileMill.editor) {
+    TileMill.editor[i]();
+  }
+});
