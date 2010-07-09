06c949e02c5fa3a85defd685660b3fbb47026555
Young Hahn <yfhahn@gmail.com>
2010-07-09 13:28:21 -0400

    Use Tom's base64 ecode with ur slafe.

--- tilemill/static/js/base64.js
+++ tilemill/static/js/base64.js
@@ -1,142 +1,48 @@
-/**
-*
-*  Base64 encode / decode
-*  http://www.webtoolkit.info/
-*
-**/
- 
-var Base64 = {
- 
-	// private property
-	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
- 
-	// public method for encoding
-	encode : function (input) {
-		var output = "";
-		var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-		var i = 0;
- 
-		input = Base64._utf8_encode(input);
- 
-		while (i < input.length) {
- 
-			chr1 = input.charCodeAt(i++);
-			chr2 = input.charCodeAt(i++);
-			chr3 = input.charCodeAt(i++);
- 
-			enc1 = chr1 >> 2;
-			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-			enc4 = chr3 & 63;
- 
-			if (isNaN(chr2)) {
-				enc3 = enc4 = 64;
-			} else if (isNaN(chr3)) {
-				enc4 = 64;
-			}
- 
-			output = output +
-			this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-			this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
- 
-		}
- 
-		return output;
-	},
- 
-	// public method for decoding
-	decode : function (input) {
-		var output = "";
-		var chr1, chr2, chr3;
-		var enc1, enc2, enc3, enc4;
-		var i = 0;
- 
-		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
- 
-		while (i < input.length) {
- 
-			enc1 = this._keyStr.indexOf(input.charAt(i++));
-			enc2 = this._keyStr.indexOf(input.charAt(i++));
-			enc3 = this._keyStr.indexOf(input.charAt(i++));
-			enc4 = this._keyStr.indexOf(input.charAt(i++));
- 
-			chr1 = (enc1 << 2) | (enc2 >> 4);
-			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-			chr3 = ((enc3 & 3) << 6) | enc4;
- 
-			output = output + String.fromCharCode(chr1);
- 
-			if (enc3 != 64) {
-				output = output + String.fromCharCode(chr2);
-			}
-			if (enc4 != 64) {
-				output = output + String.fromCharCode(chr3);
-			}
- 
-		}
- 
-		output = Base64._utf8_decode(output);
- 
-		return output;
- 
-	},
- 
-	// private method for UTF-8 encoding
-	_utf8_encode : function (string) {
-		string = string.replace(/\r\n/g,"\n");
-		var utftext = "";
- 
-		for (var n = 0; n < string.length; n++) {
- 
-			var c = string.charCodeAt(n);
- 
-			if (c < 128) {
-				utftext += String.fromCharCode(c);
-			}
-			else if((c > 127) && (c < 2048)) {
-				utftext += String.fromCharCode((c >> 6) | 192);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-			else {
-				utftext += String.fromCharCode((c >> 12) | 224);
-				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
- 
-		}
- 
-		return utftext;
-	},
- 
-	// private method for UTF-8 decoding
-	_utf8_decode : function (utftext) {
-		var string = "";
-		var i = 0;
-		var c = c1 = c2 = 0;
- 
-		while ( i < utftext.length ) {
- 
-			c = utftext.charCodeAt(i);
- 
-			if (c < 128) {
-				string += String.fromCharCode(c);
-				i++;
-			}
-			else if((c > 191) && (c < 224)) {
-				c2 = utftext.charCodeAt(i+1);
-				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-				i += 2;
-			}
-			else {
-				c2 = utftext.charCodeAt(i+1);
-				c3 = utftext.charCodeAt(i+2);
-				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-				i += 3;
-			}
- 
-		}
- 
-		return string;
-	}
- 
-}
+// Based on public domain code by Tyler Akins <http://rumkin.com/>
+// Original code at http://rumkin.com/tools/compression/base64.php
+
+var Base64 = (function() {
+  function urlsafe_encode_base64(data) {
+    return this.encode(data).replace('/', '_').replace('+', '-');
+  }
+
+  function encode_base64(data) {
+    var out = "", c1, c2, c3, e1, e2, e3, e4;
+    for (var i = 0; i < data.length; ) {
+       c1 = data.charCodeAt(i++);
+       c2 = data.charCodeAt(i++);
+       c3 = data.charCodeAt(i++);
+       e1 = c1 >> 2;
+       e2 = ((c1 & 3) << 4) + (c2 >> 4);
+       e3 = ((c2 & 15) << 2) + (c3 >> 6);
+       e4 = c3 & 63;
+       if (isNaN(c2))
+         e3 = e4 = 64;
+       else if (isNaN(c3))
+         e4 = 64;
+       out += tab.charAt(e1) + tab.charAt(e2) + tab.charAt(e3) + tab.charAt(e4);
+    }
+    return out;
+  }
+
+  function decode_base64(data) {
+    var out = "", c1, c2, c3, e1, e2, e3, e4;
+    for (var i = 0; i < data.length; ) {
+      e1 = tab.indexOf(data.charAt(i++));
+      e2 = tab.indexOf(data.charAt(i++));
+      e3 = tab.indexOf(data.charAt(i++));
+      e4 = tab.indexOf(data.charAt(i++));
+      c1 = (e1 << 2) + (e2 >> 4);
+      c2 = ((e2 & 15) << 4) + (e3 >> 2);
+      c3 = ((e3 & 3) << 6) + e4;
+      out += String.fromCharCode(c1);
+      if (e3 != 64)
+        out += String.fromCharCode(c2);
+      if (e4 != 64)
+        out += String.fromCharCode(c3);
+    }
+    return out;
+  }
+  var tab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+  return { encode:encode_base64, decode:decode_base64, urlsafe_encode:urlsafe_encode_base64 };
+})();
