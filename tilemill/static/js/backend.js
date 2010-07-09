0d703fefc4c126843e110e8b743bb62123422ab0
Young Hahn <yfhahn@gmail.com>
2010-07-09 13:28:40 -0400

    Add file, url and fields methods to backends.

--- tilemill/static/js/backend.js
+++ tilemill/static/js/backend.js
@@ -24,15 +24,44 @@ TileMill.backend.servers.python.add = function(id, type, callback) {
   });
 }
 
+TileMill.backend.servers.python.file = function(filename, callback) {
+  var cache = TileMill.cache.get('python-file', filename);
+  if (cache) {
+    callback(cache);
+  }
+  else {
+    $.get(TileMill.settings.pythonServer + 'file', { 'filename': filename }, function(data) {
+      TileMill.cache.set('python-file', filename, data);
+      callback(data);
+    });
+  }
+}
+
+TileMill.backend.servers.python.url = function(filename) {
+  return TileMill.settings.pythonServer + 'file?filename=' + filename;
+}
+
 // TileLive backend
+TileMill.backend.rasterizers.tilelive.fields = function(mmlb64, callback) {
+  var cache = TileMill.cache.get('tilelive-fields', mmlb64);
+  if (cache) {
+    callback(cache);
+  }
+  else {
+    var head = document.getElementsByTagName("head")[0], script = document.createElement("script");
+    script.src = TileMill.settings.tileliveServer.split(',')[0] + mmlb64 + "/fields.json?jsoncallback=" + callback;
+    head.insertBefore(script, head.firstChild);
+  }
+}
+
 TileMill.backend.rasterizers.tilelive.values = function(layer, field) {
   
 }
 
-$.each(['list', 'add'], function(i, func) {
+$.each(['list', 'add', 'file', 'url'], function(i, func) {
   TileMill.backend[func] = TileMill.backend.servers[TileMill.settings.server][func];
 });
 
-$.each(['values'], function(i, func) {
+$.each(['fields', 'values'], function(i, func) {
   TileMill.backend[func] = TileMill.backend.rasterizers[TileMill.settings.rasterizer][func];
 });
