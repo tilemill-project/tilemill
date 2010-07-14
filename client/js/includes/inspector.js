ee288003d9e8117a9e0237ca58072d8e3cf08899
Young Hahn <yfhahn@gmail.com>
2010-07-14 19:21:35 -0400

    Lint.

--- client/js/includes/inspector.js
+++ client/js/includes/inspector.js
@@ -1,20 +1,29 @@
+/**
+ * Field and value inspector.
+ */
 TileMill.inspector = { page: {}, inspection: {}, urls: {} };
 
+/**
+ * Init and return markup.
+ */
 TileMill.inspector.init = function() {
   var inspector = $(TileMill.template('inspector', {}));
   TileMill.inspector.load();
   return inspector;
 };
 
+/**
+ * Inspect a layer (show its fields).
+ */
 TileMill.inspector.inspect = function(id, visualize) {
   if (TileMill.inspection && TileMill.inspection[id] && TileMill.inspection[id].fields) {
-    for (field in TileMill.inspection[id].fields) {
+    for (var field in TileMill.inspection[id].fields) {
       (function(layer, field) {
         var type = TileMill.inspection[layer].fields[field];
         var li = $(TileMill.template('inspector-field', {
           field: field,
           type: type.replace('int', 'integer').replace('str', 'string'),
-          visualize: visualize,
+          visualize: visualize
         }));
         // Attach value inspector.
         $('a.inspect-values', li).click(function() {
@@ -29,8 +38,11 @@ TileMill.inspector.inspect = function(id, visualize) {
       })(id, field);
     }
   }
-}
+};
 
+/**
+ * Load inspector data from the rasterizer backend.
+ */
 TileMill.inspector.load = function(callback) {
   $('.layer-inspect').removeClass('layer-inspect').addClass('layer-inspect-loading');
   TileMill.backend.fields(TileMill.mml.url(), function(data) {
@@ -41,16 +53,19 @@ TileMill.inspector.load = function(callback) {
       callback();
     }
   });
-}
+};
 
+/**
+ * Inspect a field (show its values).
+ */
 TileMill.inspector.values = function(layer, field, pager) {
   var callback = (function(data) {
     if (data.field) {
       var values = $(TileMill.template('inspector-values', {
         min: data.min,
         max: data.max,
-        class_prev: (TileMill.inspector.page[layer][field] != 0 ? '' : ' disabled'),
-        class_next: ((data.count - (TileMill.inspector.page[layer][field] * 30) > 30) ? '' : ' disabled'),
+        class_prev: (TileMill.inspector.page[layer][field] !== 0 ? '' : ' disabled'),
+        class_next: ((data.count - (TileMill.inspector.page[layer][field] * 30) > 30) ? '' : ' disabled')
       }));
       for (var i in data.values) {
         $('ul', values).append('<li>' + data.values[i] + '</li>');
@@ -99,4 +114,4 @@ TileMill.inspector.values = function(layer, field, pager) {
       'callback': callback
     });
   }
-}
+};
