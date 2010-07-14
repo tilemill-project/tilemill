5ac93430b5d8c4e1a1864a11592beea7b8539ab1
Young Hahn <yfhahn@gmail.com>
2010-07-14 00:00:49 -0400

    Adding settings.js

---
+++ client/settings.js
@@ -0,0 +1,24 @@
+/**
+ * TileMill client configuration
+ * ============================================================================
+ * The following file is an example of settings you can specify for your
+ * particular TileMill client. Uncomment & adjust any of the following lines
+ * to suit your needs.
+ */
+
+/**
+ * Example: Use the TileMill PHP backend available at localhost.
+ */
+// TileMill.settings.server = 'simple';
+// TileMill.settings.simpleServer = 'http://localhost/tilemill/phpServer/';
+
+/**
+ * Example: Use a local TileLive rasterizer at http://localhost:8888/
+ */
+// TileMill.settings.rasterizer = 'tilelive';
+// TileMill.settings.tileliveServer = 'http://localhost:8888/';
+
+/**
+ * Example: Use the default HTML client runtime.
+ */
+// TileMill.settings.runtime = 'html';
