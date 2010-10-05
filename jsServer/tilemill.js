ed9371a6c9532a179134f858c0c69c90dd76dabd
Tom MacWright <macwright@gmail.com>
2010-10-05 19:32:30 -0400

    Getting node.js backend up to speed for the most part

--- jsServer/tilemill.js
+++ jsServer/tilemill.js
@@ -1,71 +1,99 @@
 var express = require('express'), fs = require('fs');
+var _ = require('./underscore')._;
 var app = module.exports = express.createServer();
-var files = 'projects'; // TODO: put in config
+var files = 'project'; // TODO: put in config
 
-app.get('/',function(req,res,params){res.send({'api':'basic','version':1.0});});
+function jsonp(obj, req) {
+  return req.param('jsoncallback') + '(' + JSON.stringify(obj) + ')';
+}
 
-app.get('/list', function(req, res, params) {
-  // params['filename']
-  // path = os.path.join(options.files, self.get_argument('filename'))
-  // if (self.safePath(path) and os.path.isdir(path)):
-  //     directories = []
-  //     for root, dirs, files in os.walk(path):
-  //         basename = os.path.basename(root)
-  //         if os.path.isfile(os.path.join(root, basename + '.mml')):
-  //             directories.append(basename)
-       // res.send({'status': true, 'data': directories});
-  // elif (self.safePath(path)):
-      res.send({'status':false, 'data':'The file could not be found'});
-  //else:
-      res.send({'status':false, 'data':'Invalid filename'});
+function safePath(path) {
+  // return True or path.find('..') == -1 and not re.search('[^\w.-_\/]', path)
+}
+
+app.get('/', function(req, res, params) {
+  res.send({'api':'basic','version':1.0});
 });
 
-app.get('/file', function(req, res, params) {
-  var path = files + '/' + params['filename']);
+app.get('/list', function(req, res) {
+  res.send(
+    jsonp({
+      'status': true,
+      'data': _.select(fs.readdirSync(files),
+        function(dir) {
+          return _.any(
+            fs.readdirSync(files + '/' + dir),
+            function(filename) {
+              return filename.match('.mml') 
+            }
+          )
+        }
+      )
+    }, req)
+  );
+});
+
+app.get('/file', function(req, res) {
+  var path = req.param('filename');
   try {
-    // if (path.find('..') != -1 || not re.search('[^\w.-_\/]', path))
-    //   throw Exception('Invalid filename');
-    res.send(fs.readFileSync(path));
+    res.send(jsonp("" + fs.readFileSync(path), req));
   }
   catch(Exception) {
-    res.send({'status':false, 'data':'The file could not be found'})
+    res.send(jsonp(
+    {
+        'status':false, 
+        'data':'The file (' + req.param('filename') + 
+      ') could not be found. Exception: ' + Exception
+    }, req))
   }
-  // elif (self.safePath(path)):
-  // else:
-  //     res.send({ 'status': False, 'data': 'Invalid filename' })
 });
 
-app.post('/file', function(req, res, params) {
-//    def post(self):
-//        path = os.path.join(options.files, self.get_argument('filename'))
-//        if (self.safePath(path)):
-//            method = self.get_argument('method', 'put')
-//            data = self.get_argument('data', '')
-//            if method == 'delete':
-//                self.rm(path);
-//                res.send({ 'status': True }, True)
-//            else:
-//                if (not os.path.isdir(os.path.dirname(path))):
-//                    os.makedirs(os.path.dirname(path))
-//                if (os.path.isdir(os.path.dirname(path))):
-//                    buffer = open(path, 'w')
-//                    buffer.writelines(data)
-//                    buffer.close()
-//                    res.send({ 'status': True }, True)
-//                else:
-//                    res.send({ 'status': False, 'data': 'Could not write file' }, True)
-//        elif (self.safePath(path)):
-//            self.send({'status':false, 'data':'Could not write file' });
-//        else:
-//            self.send({'status':false, 'data':'Invalid filename' });
+/*
+app.post('/file', function(req, res) {
+  path = os.path.join(options.files, self.get_argument('filename'))
+  //if (self.safePath(path)):
+  method = req.param('method')
+  data = req.param('data')
+  if (method == 'delete') {
+    this.rm(path);
+    res.send(jsonp({ 'status': True }, req))
+  }
+  else {
+    // if (os.path.isdir(os.path.dirname(path))):
+    // os.makedirs(os.path.dirname(path))
+    if (fs.fstatSync(path))) {
+      buffer = fs.writeFile(path, data, 
+        function() {
+          res.send(jsonp({ 'status': true }, req))
+        }
+      )
+    }
+    else {
+      res.send(jsonp(
+      {
+        'status': False, 
+        'data': 'Could not write file'
+      }, req)
+      )
+    }
+  }
+}
+*/
+
+app.get('/mtime', function(req, res) {
+  var path = req.param('filename');
+  try {
+    fstat = fs.statSync(path) 
+    res.send(jsonp("" + fstat.mtime, req));
+  }
+  catch(Exception) {
+    res.send(jsonp(
+    {
+        'status':false, 
+        'data':'The file (' + req.param('filename') + 
+      ') could not be found. Exception: ' + Exception
+    }, req))
+  }
 });
 
-//    def rm(self, path):
-//        for root, dirs, files in os.walk(path, topdown=False):
-//            for name in files:
-//                os.remove(os.path.join(root, name))
-//            for name in dirs:
-//                os.rmdir(os.path.join(root, name))
-//
-
-app.listen(3000);
+app.listen(8889);
