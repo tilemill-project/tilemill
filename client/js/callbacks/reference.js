77b87e1ed5f3673b7233544107e9f34ceebd4f21
Tom MacWright <macwright@gmail.com>
2010-12-10 12:14:02 -0500

    Adding reference callback, this should have been there before

---
+++ client/js/callbacks/reference.js
@@ -0,0 +1,26 @@
+/**
+ * Router controller: Reference page.
+ */
+TileMill.controller.reference = function() {
+  TileMill.show(TileMill.template('reference', {}));
+
+  $.getJSON('js/data/reference.json', {}, function(data) {
+    _.map(data.symbolizers, function(properties, symbolizer) {
+      $('#reference-content').append(TileMill.template('reference-symbolizer', {
+        properties: _.map(properties, function(property, name) {
+          return TileMill.template('reference-property', {
+            property_name: name,
+            css: property.css,
+            type: _.isArray(property.type) ?
+              property.type.join(', ') :
+              property.type,
+            default_value: property['default-value']
+          }); // TODO extend instead
+        }).join(''),
+        symbolizer: symbolizer
+      }));
+    });
+  });
+};
+
+TileMill.reference = {};
