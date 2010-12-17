d8bd9b711231489e6c48b710ac0fda41222633ae
Tom MacWright <macwright@gmail.com>
2010-12-17 14:22:46 -0500

    Removing custom rmRf function, using npm code, Fixes #30

--- server/tilemill.js
+++ server/tilemill.js
@@ -3,6 +3,7 @@ require.paths.unshift(__dirname + '/lib/node');
 var express = require('express'),
     fs = require('fs'),
     path = require('path'),
+    rmrf = require('./rm-rf'),
     _ = require('underscore')._;
 
 var settings = JSON.parse(fs.readFileSync('settings.json'));
@@ -23,9 +24,9 @@ app.get('/api/list', function(req, res) {
   res.send({
     status: true,
     data: _.select(fs.readdirSync(
-        path.join(
-            settings.files,
-            req.param('filename'))),
+      path.join(
+        settings.files,
+        req.param('filename'))),
       function(dir) {
         // directories that contain at least one MML file
         return _.any(
@@ -62,29 +63,14 @@ app.get('/api/file', function(req, res) {
   });
 });
 
-var rmRf = function(dir, callback) {
-  fs.readdir(dir, function(err, files) {
-    files.forEach(function(file) {
-      fs.stat(path.join(dir, file), function(err, stats) {
-        if (stats.isDirectory()) {
-          rmRf(file);
-        } else {
-          fs.unlink(path.join(dir, file), function(err) { });
-        }
-      });
-    });
-  });
-  callback && callback();
-};
-
 app.post('/api/file', function(req, res) {
   if ((req.param('method') || 'put') == 'put') {
     if (path.dirname(req.body.filename)) {
       fs.mkdir(
-          path.join(
-              settings.files,
-              path.dirname(path.join(req.body.filename))),
-          0777,
+        path.join(
+          settings.files,
+          path.dirname(path.join(req.body.filename))),
+        0777,
       function() {
         fs.writeFile(
             path.join(settings.files, req.body.filename),
@@ -98,7 +84,7 @@ app.post('/api/file', function(req, res) {
     }
   } else {
     // TODO: nodejs doesn't provide rm-rf functionality
-    rmRf(path.join(settings.files, req.body.filename), function() {
+    rmrf(path.join(settings.files, req.body.filename), function() {
         res.send({
             status: true
         });
@@ -111,23 +97,23 @@ app.get('/api/mtime', function(req, res) {
   var filename = req.param('filename');
   path.exists(path.join(settings.files, req.param('filename')),
     function (exists) {
-        if (!exists) {
-            res.send({
-              status: false,
-              data: 'The file (' +
-                path.join(settings.files, req.param('filename')) +
-                ') could not be found.'
-            });
-        } else {
-            fs.stat(path.join(
-                    settings.files,
-                    req.param('filename')), function(err, stats) {
-                res.send({
-                    mtime: '' + stats.mtime,
-                    filename: req.param('filename')
-                });
-            });
-        }
+      if (!exists) {
+        res.send({
+          status: false,
+          data: 'The file (' +
+            path.join(settings.files, req.param('filename')) +
+            ') could not be found.'
+        });
+      } else {
+        fs.stat(path.join(
+            settings.files,
+            req.param('filename')), function(err, stats) {
+          res.send({
+            mtime: '' + stats.mtime,
+            filename: req.param('filename')
+          });
+        });
+      }
     });
 });
 
