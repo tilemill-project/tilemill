b332be77fe89eb411c2ceb96b5de4a5d12307d96
Tom MacWright <macwright@gmail.com>
2010-12-23 15:19:35 -0500

    Attempt bootstrap

--- client/js/tilemill.js
+++ client/js/tilemill.js
@@ -11,14 +11,24 @@ var TileMill = Backbone.Controller.extend({
     },
     
     initialize: function() {
-      this._projects = new Projects;
-      this._reference = new ReferenceView;
+      var that = this;
+      $.ajax({
+          url: '/api/project',
+          dataType: 'json',
+          data: {},
+          success: function(data) {
+            that._projects = new ProjectCollection(data);
+            that._index = new ListView({
+              model: that._products
+            });
+            Backbone.history.loadUrl();
+          }
+      });
     },
 
     list: function() {
       console.log('LIST routed');
       $('#list').show();
-      this._projects.fetch();
       this._projects.map(function(p) {
         var pr = new ProjectRow({model: p});
         $('#projects-list').html(pr.render());
