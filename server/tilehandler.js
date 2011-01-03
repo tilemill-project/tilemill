bfab20dc1504d9136b614aa6c3bb0a7aba61992d
Will White <will@developmentseed.org>
2011-01-03 18:11:48 -0500

    Missed this file.

---
+++ server/tilehandler.js
@@ -0,0 +1,44 @@
+var _ = require('underscore')._,
+    Tile = require('tilelive.js');
+
+var mapnik = require('mapnik');
+mapnik.register_datasources('/usr/local/lib/mapnik2/input');
+mapnik.register_fonts('/usr/local/lib/mapnik2/fonts/');
+
+module.exports = function(app, settings) {
+    app.get('/:scheme/:mapfile_64/:z/:x/:y.*', function(req, res) {
+        /*
+         * scheme: (xyz|tms|tile (tms))
+         *
+         * format:
+         * - Tile: (png|jpg)
+         * - Data Tile: (geojson)
+         * - Grid Tile: (*.grid.json)
+         */
+        try {
+            var tile = new Tile(
+                req.params.scheme,
+                req.params.mapfile_64,
+                req.params.z,
+                req.params.x,
+                req.params.y,
+                req.params[0],
+                settings.mapfile_dir);
+        } catch (err) {
+            res.send('Tile invalid: ' + err.message);
+        }
+
+        tile.render(function(err, data) {
+            if (!err) {
+                // Using apply here allows the tile rendering
+                // function to send custom heades without access
+                // to the request object.
+                data[1] = _.extend(settings.header_defaults, data[1]);
+                res.send.apply(res, data);
+                // res.send.apply(res, ['hello', { 'Content-Type': 'image/png' }]);
+            } else {
+                res.send('Tile rendering error: ' + err);
+            }
+        });
+    });
+}
