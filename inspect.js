2d01d8b85846157e0f8e2ce7b1fa11cb7e22dae1
Young Hahn <young@developmentseed.org>
2011-01-04 11:19:59 -0500

    Send dummy payloads at inspection callbacks.

--- inspect.js
+++ inspect.js
@@ -14,24 +14,28 @@ module.exports = function(app, settings) {
      * Inspect fields
      */
     app.get('/:mapfile_64/fields.json', function(req, res) {
+        res.send({});
     });
 
     /**
      * Inspect data
      */
     app.get('/:mapfile_64/data.json', function(req, res) {
+        res.send({});
     });
 
     /**
      * Inspect layer
      */
     app.get('/:mapfile_64/:layer_64/layer.json', function(req, res) {
+        res.send({});
     });
 
     /**
      * Inspect field values
      */
     app.get('/:mapfile_64/:layer_64/:feature_64/values.json', function(req, res) {
+        res.send({});
     });
 }
 
