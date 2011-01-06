411ca392c07564ef245f6f26dcb4d2044bc3ffcb
Young Hahn <young@developmentseed.org>
2011-01-05 19:20:05 -0500

    Fix for last commit.

--- client/js/app.project.js
+++ client/js/app.project.js
@@ -1,6 +1,7 @@
 var Project = Backbone.Model.extend({
     SRS_DEFAULT: '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs',
     initialize: function() {
+        var self = this;
         // Set default values.
         if (!this.get('srs')) {
             this.set({'srs': this.SRS_DEFAULT});
