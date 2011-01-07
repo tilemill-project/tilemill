db5d79f05035b4754ae24f1485df53a0dbd41a47
Will White <will@developmentseed.org>
2011-01-07 10:49:35 -0500

    Add color swatch collection and model.

--- client/js/app.stylesheet.js
+++ client/js/app.stylesheet.js
@@ -129,7 +129,10 @@ var StylesheetTabView = Backbone.View.extend({
         var colorPicker = new ColorPickerToolView({
             model: this.model
         });
-        var colorSwatches = new ColorSwatchesToolView();
+        var colorSwatches = new ColorSwatchesToolView({
+            collection: new ColorSwatchesList(null, { parent: this }),
+            parent: this,
+        });
         var fontPicker = new FontPickerToolView({
             model: new Abilities,
             parent: this
@@ -168,12 +171,9 @@ var StylesheetTabView = Backbone.View.extend({
                 },
                 onChange: function() {
                     self.model.collection.parent.change();
-                    // @TODO need an event that the color picker (and other
-                    // editor "plugins" can bind to.
                 },
                 initCallback: function(cm) {
-                    // @TODO need an event that the color picker (and other
-                    // editor "plugins" can bind to.
+                    self.model.collection.parent.trigger('ready');
                 }
             });
         }
@@ -215,7 +215,8 @@ var ColorPickerToolView = Backbone.View.extend({
         $(this.el).html(ich.ColorPickerToolView);
     },
     activate: function() {
-        this.$('.tilemill-farbtastic').farbtastic({
+        var farb = $('.tilemill-farbtastic', this.el);
+        this.farbtastic = $.farbtastic(farb, {
             callback: 'input.color',
             width: 200,
             height: 200
@@ -227,9 +228,85 @@ var ColorPickerToolView = Backbone.View.extend({
     }
 });
 
+var ColorSwatch = Backbone.Model.extend({
+    initialize: function() {
+        this.set({ hsl: this.RGBToHSL(this.unpack(this.get('hex'))) });
+    },
+    /* From farbtastic */
+    RGBToHSL: function (rgb) {
+      var r = rgb[0], g = rgb[1], b = rgb[2],
+          min = Math.min(r, g, b),
+          max = Math.max(r, g, b),
+          delta = max - min,
+          h = 0,
+          s = 0,
+          l = (min + max) / 2;
+      if (l > 0 && l < 1) {
+        s = delta / (l < 0.5 ? (2 * l) : (2 - 2 * l));
+      }
+      if (delta > 0) {
+        if (max == r && max != g) h += (g - b) / delta;
+        if (max == g && max != b) h += (2 + (b - r) / delta);
+        if (max == b && max != r) h += (4 + (r - g) / delta);
+        h /= 6;
+      }
+      return [h, s, l];
+    },
+    unpack: function (color) {
+      if (color.length == 7) {
+        function x(i) {
+          return parseInt(color.substring(i, i + 2), 16) / 255;
+        }
+        return [ x(1), x(3), x(5) ];
+      }
+      else if (color.length == 4) {
+        function x(i) {
+          return parseInt(color.substring(i, i + 1), 16) / 15;
+        }
+        return [ x(1), x(2), x(3) ];
+      }
+    },
+    url: function() {
+        return '/foo';
+    }
+});
+
+var ColorSwatchesList = Backbone.Collection.extend({
+    model: ColorSwatch,
+    initialize: function(models, options) {
+        this.parent = options.parent;
+        _.bindAll(this, 'reload');
+        this.parent.model.collection.parent.bind('change', this.reload);
+        this.parent.model.collection.parent.bind('ready', this.reload);
+    },
+    reload: function() {
+        var matches = this.parent.model.collection.pluck('data').join('\n').match(/\#[A-Fa-f0-9]{3,6}/g);
+        if (matches) {
+            // Clear collection
+            var coll = this;
+            this.forEach(function(color) {
+                coll.remove(color);
+            });
+            for (var i = 0; i < matches.length; i++) {
+                var pass = false;
+                this.forEach(function(color) {
+                    if (color.get('hex') == matches[i]) {
+                        pass = true;
+                    }
+                });
+                if (!pass) {
+                    this.add(new ColorSwatch({hex: matches[i]}));
+                }
+            }
+        }
+    }
+});
+
 var ColorSwatchesToolView = Backbone.View.extend({
     id: 'color-swatches',
-    initialize: function() {
+    initialize: function(options) {
+        _.bindAll(this, 'render', 'activate', 'reload');
+        this.parent = options.parent;
         this.render();
     },
     render: function() {
