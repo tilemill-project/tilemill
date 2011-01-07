db0782edd130a0f66004b08ec3797b74221d5c8d
Will White <will@developmentseed.org>
2011-01-07 16:31:50 -0500

    Remove log message.

--- client/js/app.tools.js
+++ client/js/app.tools.js
@@ -78,7 +78,6 @@ var ColorSwatchesList = Backbone.Collection.extend({
     },
     reload: function() {
         var matches = this.project.get('Stylesheet').pluck('data').join('\n').match(/\#[A-Fa-f0-9]{6}\b|\#[A-Fa-f0-9]{3}\b/g);
-        console.log(matches);
         if (!matches) {
             matches = [];
         }
