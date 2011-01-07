9cdf9ba0b09dd600cd719393e62bc16faa14a899
Will White <will@developmentseed.org>
2011-01-07 16:31:02 -0500

    Better swatch handling.

--- client/js/app.tools.js
+++ client/js/app.tools.js
@@ -30,7 +30,9 @@ var ColorSwatch = Backbone.Model.extend({
     initialize: function() {
         this.set({ hsl: this.RGBToHSL(this.unpack(this.get('hex'))) });
     },
-    /* From farbtastic */
+    /**
+     * From farbtastic.
+     */
     RGBToHSL: function (rgb) {
       var r = rgb[0], g = rgb[1], b = rgb[2],
           min = Math.min(r, g, b),
@@ -75,27 +77,42 @@ var ColorSwatchesList = Backbone.Collection.extend({
         this.project.bind('ready', this.reload);
     },
     reload: function() {
-        var matches = this.project.get('Stylesheet').pluck('data').join('\n').match(/\#[A-Fa-f0-9]{3,6}/g);
-        if (matches) {
-            // Clear collection
-            var coll = this;
-            this.forEach(function(color) {
-                coll.remove(color);
-            });
-            for (var i = 0; i < matches.length; i++) {
-                var pass = false;
-                this.forEach(function(color) {
-                    if (color.get('hex') == matches[i]) {
-                        pass = true;
-                    }
-                });
-                if (!pass) {
-                    this.add(new ColorSwatch({hex: matches[i]}), {silent: true});
-                }
-            }
-            // Trigger the deferred add event
-            this.trigger('add');
+        var matches = this.project.get('Stylesheet').pluck('data').join('\n').match(/\#[A-Fa-f0-9]{6}\b|\#[A-Fa-f0-9]{3}\b/g);
+        console.log(matches);
+        if (!matches) {
+            matches = [];
         }
+        matches = _.uniq(matches);
+
+        var hexMap = {}
+        this.each(function(swatch) { hexMap[swatch.get('hex')]  = swatch; });
+
+        // Get list of models to remove.
+        var remove = _.without.apply(this, [this.pluck('hex')].concat(matches));
+        var coll = this;
+        _.each(remove, function(hex) {
+            coll.remove(hexMap[hex]);
+        })
+
+        for (var i = 0; i < matches.length; i++) {
+            var pass = false;
+            this.forEach(function(color) {
+                if (color.get('hex') == matches[i]) {
+                    pass = true;
+                }
+            });
+            if (!pass) {
+                this.add(new ColorSwatch({hex: matches[i]}), {silent: true});
+            }
+        }
+        // Trigger the deferred add event
+        this.trigger('add');
+    },
+    /**
+     * Sort swatches by lightness.
+     */
+    comparator: function(swatch) {
+        return swatch.get('hsl')[2];
     }
 });
 
