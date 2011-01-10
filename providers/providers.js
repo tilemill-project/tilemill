6711d04208c3ae7e5ea1a374e2cbd22f480821ae
Young Hahn <young@developmentseed.org>
2011-01-10 00:05:09 -0500

    Style cleanup and better (but not finished) error handling for bad provider requests.

--- providers/providers.js
+++ providers/providers.js
@@ -2,20 +2,29 @@ var path = require('path');
 var _ = require('underscore')._;
 
 module.exports = function(app, settings) {
-  var providers = {};
-  for (var p in settings.providers) {
-      // TODO :express better
-      providers[p] = require('./' + path.join(p, 'index'))(app, settings);
-  }
-  app.get('/provider', function(req, res) {
-      res.send({
-          status: true,
-          provider: _.keys(settings.providers)
-      });
-  });
-  app.get('/provider/:provider', function(req, res) {
-      providers[req.params.provider].objects(function(objects) {
-          res.send(objects);
-      });
-  });
+    var providers = {};
+    for (var p in settings.providers) {
+        // TODO :express better
+        providers[p] = require('./' + path.join(p, 'index'))(app, settings);
+    }
+    app.get('/provider', function(req, res) {
+        res.send({
+            status: true,
+            provider: _.keys(settings.providers)
+        });
+    });
+    app.get('/provider/:provider', function(req, res) {
+        if (providers[req.params.provider]) {
+            providers[req.params.provider].objects(function(objects) {
+                res.send(objects);
+            });
+        }
+        else {
+            // @TODO: better error handling here.
+            // Currently allows graceful downgrade of missing S3 information
+            // but ideally client side JS would not request for the S3 provider
+            // at all if it is not enabled.
+            res.send([]);
+        }
+    });
 };
