775a26c70c31b3cdb802d2b1e6a47b3ed598d15b
Young Hahn <young@developmentseed.org>
2011-01-16 01:45:01 -0500

    Initial attempt at using backbone models, collections server side.

---
+++ project.js
@@ -0,0 +1,208 @@
+// Require Backbone, if we're on the server, and it's not already present.
+var Backbone = this.Backbone;
+if (!Backbone && (typeof require !== 'undefined')) Backbone = require('./backbone');
+
+/**
+ * Model: Stylesheet
+ *
+ * This model is *not* backed directly by the server.
+ * It is a child model of Project and is saved serialized as part of the parent
+ * Project model.
+ */
+var Stylesheet = Backbone.Model.extend({
+    initialize: function() {
+        if (!this.get('data')) {
+            this.set({ 'data': '' });
+        }
+    },
+    validate: function(attributes) {
+        if (/^[a-z0-9\-_.]+$/i.test(attributes.id) === false) {
+            return 'Name must contain only letters, numbers, dashes, underscores and periods.';
+        }
+    }
+});
+
+/**
+ * Collection: StylesheetList
+ *
+ * This collection is *not* backed directly by the server.
+ * This collection is a child of the Project model. When it is updated
+ * (add/remove events) it updates the attributes of its parent model as well.
+ */
+var StylesheetList = Backbone.Collection.extend({
+    model: Stylesheet,
+    initialize: function(models, options) {
+        var that = this;
+        this.parent = options.parent;
+        this.bind('change', function() {
+            this.parent.set({ 'Stylesheet': that });
+            this.parent.change();
+        });
+        this.bind('add', function() {
+            this.parent.set({ 'Stylesheet': that });
+            this.parent.change();
+        });
+        this.bind('remove', function() {
+            this.parent.set({ 'Stylesheet': that });
+            this.parent.change();
+        });
+    }
+});
+
+var Layer = Backbone.Model.extend({
+    // @TODO either as a feature or a bug, object attributes are not set
+    // automatically when passed to the constructor. We set it manually here.
+    initialize: function(attributes) {
+        this.set({'Datasource': attributes.Datasource});
+    },
+    validate: function(attributes) {
+        if (/^[a-z0-9\-_]+$/i.test(attributes.id) === false) {
+            return 'ID must contain only letters, numbers, dashes, and underscores.';
+        }
+        if (attributes['class'] && /^[a-z0-9\-_ ]+$/i.test(attributes['class']) === false) {
+            return 'Class must contain only letters, numbers, dashes, and underscores.';
+        }
+    }
+});
+
+var LayerFields = Backbone.Model.extend({
+    // @TODO either as a feature or a bug, object attributes are not set
+    // automatically when passed to the constructor. We set it manually here.
+    initialize: function(attributes, options) {
+        this.set({'fields': attributes.fields});
+        this.project = options.project;
+    },
+    url: function() {
+        return '/' + this.project.project64({ signed: true }) + '/' + this.id;
+    }
+});
+
+var LayerList = Backbone.Collection.extend({
+    model: Layer,
+    initialize: function(models, options) {
+        var self = this;
+        this.parent = options.parent;
+        this.bind('change', function() {
+            this.parent.set({ 'Layer': self });
+            this.parent.change();
+        });
+        this.bind('add', function() {
+            this.parent.set({ 'Layer': self });
+            this.parent.change();
+        });
+        this.bind('remove', function() {
+            this.parent.set({ 'Layer': self });
+            this.parent.change();
+        });
+    },
+});
+
+var Project = Backbone.Model.extend({
+    SRS_DEFAULT: '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 '
+    + '+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs',
+    STYLESHEET_DEFAULT: [{
+        id: 'style.mss',
+        data: 'Map {\n'
+            + '  background-color: #fff;\n'
+            + '}\n\n'
+            + '#world {\n'
+            + '  polygon-fill: #eee;\n'
+            + '  line-color: #ccc;\n'
+            + '  line-width: 0.5;\n'
+            + '}'
+    }],
+    LAYER_DEFAULT: [{
+        id: 'world',
+        name: 'world',
+        srs: '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 '
+        + '+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs',
+        Datasource: {
+            file: 'http://tilemill-data.s3.amazonaws.com/world_borders_merc.zip',
+            type: 'shape',
+            estimate_extent: 'true',
+            id: 'world'
+        }
+    }],
+    initialize: function(attributes) {
+        if (!this.get('srs')) {
+            this.set({'srs': this.SRS_DEFAULT});
+        }
+        if (!this.get('Stylesheet')) {
+            this.set({
+                'Stylesheet': new StylesheetList(this.STYLESHEET_DEFAULT, {
+                    parent: this
+                })
+            });
+        }
+        if (!this.get('Layer')) {
+            this.set({
+                'Layer': new LayerList(this.LAYER_DEFAULT, {
+                    parent: this
+                })
+            });
+        }
+    },
+    parse: function(response) {
+        var self = this;
+        // Instantiate StylesheetList and LayerList collections from JSON lists
+        // of plain JSON objects.
+        response.Stylesheet = new StylesheetList(response.Stylesheet ?
+                response.Stylesheet :
+                [], { parent: this });
+        response.Layer = new LayerList(response.Layer ?
+                response.Layer : [], { parent: this });
+        return response;
+    },
+    // Override url() method for convenience so we don't always need a
+    // collection reference around for CRUD operations on a single model.
+    url: function() {
+        return '/api/project/' + this.id;
+    },
+    /**
+     * Base64 encode this project's MML URL.
+     */
+    project64: function(options) {
+        // `window.location.origin` is not available in all browsers like
+        // Firefox. @TODO This approach won't allow TileMill to be installed in
+        // a subdirectory. Fix.
+        var url = [
+            window.location.protocol,
+            window.location.host].join('//') + this.url();
+        if (options.signed) {
+            var md5 = new MD5();
+            url += '?' + md5.digest(JSON.stringify(this)).substr(0, 6);
+        }
+        return Base64.urlsafe_encode(url);
+    },
+    /**
+     * Layer URL based on the model URL.
+     */
+    layerURL: function(options) {
+        return [window.location.protocol, window.location.host].join('//')
+            + '/tile/'
+            + this.project64(options)
+            + '/${z}/${x}/${y}.png';
+    },
+    validate: function(attributes) {
+        if (/^[a-z0-9\-_]+$/i.test(attributes.id) === false) {
+            return 'Name must contain only letters, numbers, dashes, '
+                + 'and underscores.';
+        }
+    }
+});
+
+var ProjectList = Backbone.Collection.extend({
+    model: Project,
+    url: '/api/project',
+    comparator: function(project) {
+        return project.get('id');
+    }
+});
+
+if (typeof module !== 'undefined') {
+    module.exports = {
+        Project: Project,
+        ProjectList: ProjectList
+    };
+}
+
