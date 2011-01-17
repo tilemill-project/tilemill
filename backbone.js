1c9e8415676caa67b831d92c30cd97a140f6ad91
Young Hahn <young@developmentseed.org>
2011-01-16 23:09:04 -0500

    Use Step() for managing async operations rather than event emitters (gross).

--- backbone.js
+++ backbone.js
@@ -7,7 +7,6 @@ var _ = require('underscore')._,
     settings = require('./settings'),
     rmrf = require('./rm-rf'),
     fs = require('fs'),
-    events = require('events'),
     Step = require('step'),
     path = require('path');
 
@@ -19,19 +18,29 @@ Backbone.sync = function(method, model, success, error) {
     switch (method) {
     case 'read':
         if (model.id) {
-            load(model, success, error);
+            load(model, function(err, model) {
+                return err ? error(err) : success(model);
+            });
         } else {
-            loadAll(success, error);
+            loadAll(function(err, model) {
+                return err ? error(err) : success(model);
+            });
         }
         break;
     case 'create':
-        create(model, success, error);
+        create(model, function(err, model) {
+            return err ? error(err) : success(model);
+        });
         break;
     case 'update':
-        update(model, success, error);
+        update(model, function(err, model) {
+            return err ? error(err) : success(model);
+        });
         break;
     case 'delete':
-        destroy(model, success, error);
+        destroy(model, function(err, model) {
+            return err ? error(err) : success(model);
+        });
         break;
     }
 };
@@ -39,45 +48,44 @@ Backbone.sync = function(method, model, success, error) {
 /**
  * Load a single model. Requires that model.id be populated.
  */
-function load(model, success, error) {
+function load(model, callback) {
     var projectPath = path.join(settings.files, 'project', model.id);
     fs.readFile(path.join(projectPath, model.id + '.mml'), 'utf-8',
     function(err, data) {
-        if (err || !data) {
-            return error(new Error('Error reading project file.'));
+        if (err || !data || !JSON.parse(data)) {
+            return callback(new Error('Error reading project file.'));
         }
         var object = JSON.parse(data);
-        if (data && object) {
-            // Set the object ID explicitly for multiple-load scenarios where
-            // model parse()/set() is bypassed.
-            object.id = model.id;
-            if (object.Stylesheet && object.Stylesheet.length > 0) {
-                var queue = new events.EventEmitter;
-                var queueLength = object.Stylesheet.length;
-                _.each(object.Stylesheet, function(filename, key) {
-                    fs.readFile(path.join(projectPath, filename),
-                    'utf-8',
-                    function(err, data) {
-                        object.Stylesheet[key] = {
-                            id: filename,
-                            data: data
-                        };
-                        queueLength--;
-                        if (queueLength === 0) {
-                            queue.emit('complete');
+        // Set the object ID explicitly for multiple-load scenarios where
+        // model parse()/set() is bypassed.
+        object.id = model.id;
+        if (object.Stylesheet && object.Stylesheet.length > 0) {
+            Step(
+                function() {
+                    var group = this.group();
+                    _.each(object.Stylesheet, function(filename, index) {
+                        fs.readFile(
+                            path.join(projectPath, filename),
+                            'utf-8',
+                            group()
+                        );
+                    });
+                },
+                function(err, files) {
+                    _.each(object.Stylesheet, function(filename, index) {
+                        if (files[index]) {
+                            object.Stylesheet[index] = {
+                                id: filename,
+                                data: files[index]
+                            };
                         }
                     });
-                });
-                queue.on('complete', function() {
-                    return success(object);
-                });
-            }
-            else {
-                return success(object);
-            }
+                    return callback(null, object);
+                }
+            );
         }
         else {
-            return error(new Error('Error parsing project JSON.'));
+            return callback(null, object);
         }
     });
 };
@@ -85,78 +93,79 @@ function load(model, success, error) {
 /**
  * Load an array of all models.
  */
-function loadAll(success, error) {
-    var loaded = [];
-    var queue = new events.EventEmitter;
+function loadAll(callback) {
     var basepath = path.join(settings.files, 'project');
-    path.exists(basepath, function(exists) {
-        if (!exists) fs.mkdirSync(basepath, 0777);
-        fs.readdir(basepath, function(err, files) {
+    Step(
+        function() {
+            path.exists(basepath, this);
+        },
+        function(exists) {
+            if (!exists) {
+                fs.mkdir(basepath, 0777, this);
+            }
+            else {
+                this();
+            }
+        },
+        function() {
+            fs.readdir(basepath, this);
+        },
+        function(err, files) {
             if (err) {
-                return error(new Error('Error reading projects directory.'));
+                return this(new Error('Error reading projects directory.'));
             }
             else if (files.length === 0) {
-                return success(loaded);
+                return this();
             }
-            var queueLength = files.length;
+            var group = this.group();
             for (var i = 0; i < files.length; i++) {
-                load(
-                    {id: files[i]},
-                    function(model) {
-                        loaded.push(model);
-                        queueLength--;
-                        if (queueLength === 0) {
-                            queue.emit('complete');
-                        }
-                    },
-                    function(err) {
-                        queueLength--;
-                        if (queueLength === 0) {
-                            queue.emit('complete');
-                        }
-                    }
-                );
+                load({id: files[i]}, group());
             }
-        });
-    });
-    queue.on('complete', function() {
-        success(loaded);
-    });
+        },
+        function(err, projects) {
+            return callback(err, projects);
+        }
+    );
 };
 
 /**
  * Create a new model.
  */
-function create(model, success, error) {
+function create(model, callback) {
     // @TODO assign a model id if not present.
-    save(model, success, error);
+    save(model, callback);
 };
 
 /**
  * Update an existing model.
  */
-function update(model, success, error) {
-    save(model, success, error);
+function update(model, callback) {
+    save(model, callback);
 };
 
 /**
  * Destroy (delete, remove, etc.) a model.
  */
-function destroy(model, success, error) {
+function destroy(model, callback) {
     var projectPath = path.join(settings.files, 'project', model.id);
     rmrf(projectPath, function() {
-        return success(model);
+        return callback(null, model);
     });
 };
 
 /**
  * Save a model. Called by create/update.
  */
-function save(model, success, error) {
+function save(model, callback) {
     var projectPath = path.join(settings.files, 'project', model.id);
-
-    rmrf(projectPath, function() {
-        fs.mkdir(projectPath, 0777, function() {
+    Step(
+        function() {
+            rmrf(projectPath, this);
+        },
+        function() {
+            fs.mkdir(projectPath, 0777, this);
+        },
+        function() {
             // Hard clone the model JSON before doing adjustments to the data
             // based on writing separate stylesheets.
             var data = JSON.parse(JSON.stringify(model.toJSON()));
@@ -180,23 +189,19 @@ function save(model, success, error) {
                 data: JSON.stringify(data)
             });
 
-            var queue = new events.EventEmitter;
-            var queueLength = files.length;
+            var group = this.group();
             for (var i = 0; i < files.length; i++) {
-                fs.writeFile(path.join(projectPath, files[i].filename),
+                fs.writeFile(
+                    path.join(projectPath, files[i].filename),
                     files[i].data,
-                    function(err) {
-                    queueLength--;
-                    if (queueLength === 0) {
-                        queue.emit('complete');
-                    }
-                });
+                    group()
+                );
             }
-            queue.on('complete', function() {
-                success(model);
-            });
-        });
-    });
+        },
+        function() {
+            callback(null, model);
+        }
+    );
 }
 
 module.exports = Backbone;
