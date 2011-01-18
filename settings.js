a0357e31329826d64e8274cb2067eb318c49c18c
Konstantin Käfer <github@kkaefer.com>
2011-01-18 10:33:08 -0500

    added settings file

---
+++ settings.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "port": 8889,
+    "files": "files",
+    "providers": {
+        "directory": {
+            "path": "files/local_data"
+        }
+    },
+    'mapfile_dir': 'files/mapfiles',
+    'data_dir': 'files/data',
+    // TODO: request-specific overrides
+    'header_defaults': {
+        'Expires': new Date(Date.now() +
+            1000 // second
+            * 60 // minute
+            * 60 // hour
+            * 24 // day
+            * 365 // year
+            )
+    }
+}
