1790731b9fee987bbad90cee58bdbfa937589a94
Young Hahn <young@developmentseed.org>
2011-01-25 18:26:00 -0500

    Set layer geometry for default layer.

--- shared/project.js
+++ shared/project.js
@@ -182,6 +182,7 @@ var Project = Backbone.Model.extend({
         name: 'world',
         srs: '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 '
         + '+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs',
+        geometry: 'polygon',
         Datasource: {
             file: 'http://tilemill-data.s3.amazonaws.com/world_borders_merc.zip',
             type: 'shape'
