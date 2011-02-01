1346a3b2f4f1d985bb3ff23b38b97945ca7a8376
Young Hahn <young@developmentseed.org>
2011-02-01 13:19:15 -0500

    Implement a wait until node-dirty db has been loaded.

--- server/backbone-dirty.js
+++ server/backbone-dirty.js
@@ -1,51 +1,66 @@
-var _ = require('underscore')._,
-    Backbone = require('../modules/backbone/backbone.js'),
+var Backbone = require('../modules/backbone/backbone.js'),
     settings = require('settings'),
     path = require('path'),
-    dirty = require('node-dirty')(path.join(settings.files, 'app.db'));
+    dirty = require('node-dirty')(path.join(settings.files, 'app.db')),
+    loaded = false;
 
-/**
- * Override Backbone.sync() for the server-side context.
- * Uses node-dirty for model persistence.
- */
+// Set a loaded flag to indicate whether Backbone.sync can begin accessing the
+// db immediately or must wait until the 'load' event is emitted.
+dirty.on('load', function() {
+    loaded = true;
+});
+
+// Override Backbone.sync() for the server-side context. Uses node-dirty for
+// model persistence.
 Backbone.sync = function(method, model, success, error) {
-    switch (method) {
-    case 'read':
-        if (model.id) {
-            var data = dirty.get(model.type + ':' + model.id);
-            if (data) {
-                success(data);
-            } else {
-                error('Model not found.');
-            }
-        } else {
-            var data = [];
-            dirty.forEach(function(key, val) {
-                if (model.type === key.split(':')[0] && val && data.indexOf(val) === -1) {
-                    data.push(val);
+    var sync = function(method, model, success, error) {
+        switch (method) {
+        case 'read':
+            if (model.id) {
+                var data = dirty.get(model.type + ':' + model.id);
+                if (data) {
+                    success(data);
+                } else {
+                    error('Model not found.');
                 }
-            });
-            success(data);
+            } else {
+                var data = [];
+                dirty.forEach(function(key, val) {
+                    if (model.type === key.split(':')[0] && val && data.indexOf(val) === -1) {
+                        data.push(val);
+                    }
+                });
+                success(data);
+            }
+            break;
+        case 'create':
+        case 'update':
+            dirty.set(
+                model.type + ':' + model.id,
+                model.toJSON(),
+                function(err, model) {
+                    return err ? error(err) : success(model);
+                }
+            );
+            break;
+        case 'delete':
+            dirty.rm(
+                model.type + ':' + model.id,
+                function(err, model) {
+                    return err ? error(err) : success(model);
+                }
+            );
+            break;
         }
-        break;
-    case 'create':
-    case 'update':
-        dirty.set(
-            model.type + ':' + model.id,
-            model.toJSON(),
-            function(err, model) {
-                return err ? error(err) : success(model);
-            }
-        );
-        break;
-    case 'delete':
-        dirty.rm(
-            model.type + ':' + model.id,
-            function(err, model) {
-                return err ? error(err) : success(model);
-            }
-        );
-        break;
+    };
+
+    // Wait until the node-dirty records have been fully loaded.
+    if (loaded) {
+        sync(method, model, success, error);
+    } else {
+        dirty.on('load', function() {
+            sync(method, model, success, error);
+        });
     }
 };
 
