f71c2ac022e491414ebd7885a86b55b572bd3b0c
Young Hahn <young@developmentseed.org>
2011-02-07 01:01:14 -0500

    Add screenshots/thumbnails.

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Application bootstrap.
 $(function() {
+    // Convert any markdown sections to HTML.
     $('.md').each(function() {
         var text = $(this).html();
         text = (new Showdown.converter()).makeHtml(text);
         $(this).html(text);
     });
+
+    // Set facebox paths.
+    $.facebox.settings.closeImage = 'facebox/src/closelabel.png'
+    $.facebox.settings.loadingImage = 'facebox/src/loading.gif'
+
+    $('.screenshots a').facebox();
 });
 
