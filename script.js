2886a71ccd7a0af45f46627347bd013059bf5562
Young Hahn <young@developmentseed.org>
2011-02-10 11:23:39 -0500

    Add manual.html.

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ $(function() {
             .attr('class', $(this).attr('class'))
             .attr('id', $(this).attr('id'));
         $(this).hide().after(html);
+        $('h1, h2, h3, h4, h5, h6', html).each(function() {
+            var cleaned = $(this).text().replace(/[\s\W]+/g, '-').toLowerCase();
+            $(this).attr('id', cleaned);
+        });
     });
 
     // Set facebox paths.
