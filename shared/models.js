70aea79915343e984dd95e20a1b9ed514074b553
Young Hahn <young@developmentseed.org>
2011-02-11 18:33:12 -0500

    Add interactivity settings to project settings popup.

--- shared/models.js
+++ shared/models.js
@@ -337,6 +337,9 @@ var Project = Backbone.Model.extend({
             },
             '_center': {
                 'type': 'object'
+            },
+            '_interactivity': {
+                'type': 'object'
             }
         }
     },
@@ -366,6 +369,7 @@ var Project = Backbone.Model.extend({
     defaults: {
         '_center': { lat:0, lon:0, zoom:2 },
         '_format': 'png',
+        '_interactivity': {},
         'srs': '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 '
             + '+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs',
         'Stylesheet': [],
