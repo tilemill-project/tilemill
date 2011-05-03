23f887ccecf7b7b57bba36e59a8ba5d45ba7fd7a
Will White <will@developmentseed.org>
2011-05-03 14:07:35 -0400

    Signed tile URLs for cache bypass.

--- client/js/project-map.js
+++ client/js/project-map.js
@@ -28,8 +28,11 @@ var MapView = Backbone.View.extend({
         window.app.unbind('ready', this.activate);
         var mm = com.modestmaps;
         this.map = new mm.Map('map-preview-' + this.mapID,
-            new com.modestmaps.WaxProvider({
+            new com.modestmaps.SignedProvider({
                 baseUrl: window.app.baseURL(),
+                filetype: '.' + this.model.get('_format'),
+                zoomRange: [0, 22],
+                signature: this.model.get('_updated'),
                 layerName: this.model.id}))
             .interaction()
             .legend()
@@ -57,24 +60,53 @@ var MapView = Backbone.View.extend({
         this.$('.zoom-display .zoom').text(this.map.getZoom());
     },
     reload: function() {
-        if (this.map.layers && this.map.layers && this.map.layers[0]) {
-            this.map.layers[0].type = this.model.get('_format');
-            this.map.layers[0].signature = this.model.get('_updated');
-            this.map.layers[0].redraw();
-            this.map.events.triggerEvent('changelayer', {
-                layer: this.map.layers[0],
-                property: 'visibility'
-            });
+        if (this.map) {
+            this.map.provider.filetype = '.' + this.model.get('_format');
+            this.map.provider.signature = this.model.get('_updated');
+            this.map.setProvider(this.map.provider);
         }
     }
 });
 
-// Extend OpenLayers.Layer.TMS to allow for a query-string signed URL based
+// Extend Modest Maps WaxProvider to allow for a query-string signed URL based
 // on the last updated time of the project.
-OpenLayers.Layer.SignedTMS = OpenLayers.Class(OpenLayers.Layer.TMS, {
-    getURL: function(bounds) {
-        var url = OpenLayers.Layer.TMS.prototype.getURL.call(this, bounds);
+if (!com) {
+    var com = { };
+    if (!com.modestmaps) {
+        com.modestmaps = { };
+    }
+}
+
+com.modestmaps.SignedProvider = function(options) {
+    this.layerName = options.layerName;
+    this.baseUrls = (typeof(options.baseUrl) == 'string') ?
+            [options.baseUrl] : options.baseUrl;
+    this.n_urls = this.baseUrls.length;
+    this.filetype = options.filetype || '.png';
+    this.zoomRange = options.zoomRange || [0, 18];
+    this.signature = options.signature || null;
+};
+
+com.modestmaps.SignedProvider.prototype = {
+    getTileUrl: function(coord) {
+        var server;
+        coord = this.sourceCoordinate(coord);
+        if (!coord) {
+            return null;
+        }
+
+        var worldSize = Math.pow(2, coord.zoom);
+        coord.row = Math.pow(2, coord.zoom) - coord.row - 1;
+        if (this.n_urls === 1) {
+            server = this.baseUrls[0];
+        } else {
+            server = this.baseUrls[parseInt(worldSize * coord.row + coord.column, 10) % this.n_urls];
+        }
+        var imgPath = ['1.0.0', this.layerName, coord.zoom, coord.column, coord.row].join('/');
+        var url = server + imgPath + this.filetype;
         (this.signature) && (url += '?updated=' + this.signature);
         return url;
     }
-});
+};
+
+com.modestmaps.extend(com.modestmaps.SignedProvider, com.modestmaps.WaxProvider);
