f63f56551fff0804a5441fd3d64e115703a1c15a
Dane Springmeyer <dane@dbsgeo.com>
2011-05-12 11:35:44 -0700

    add +wktext to spherical mercator proj4 string

--- shared/models.js
+++ shared/models.js
@@ -243,7 +243,8 @@ var Layer = Backbone.Model.extend({
     },
     // Constant. Hash of simple names to known SRS strings.
     SRS: {
-        '900913': '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over',
+        // note: 900913 should be the same as EPSG 3857
+        '900913': '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over',
         'WGS84': '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
     },
     // Get the name of a model's SRS string if known, otherwise reteurn
