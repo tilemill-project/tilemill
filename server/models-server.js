e69df66bb36887be6f078834ef1aa4a12577876e
Young Hahn <young@developmentseed.org>
2011-05-26 03:21:54 -0400

    Clean up project sync.

--- server/models-server.js
+++ server/models-server.js
@@ -20,11 +20,6 @@ models.Project.prototype.sync = function(method, model, success, error) {
     switch (method) {
     case 'read':
         if (model.id) {
-            // Loading a project model is not trivial in cost like backbone
-            // dirty models. We set a flag to indicate that this project has
-            // been fetched for any callers who might want to reduce the
-            // number of fetch calls made if possible.
-            model._fetched = true;
             loadProject(model, function(err, model) {
                 return err ? error(err) : success(model);
             });
@@ -48,212 +43,189 @@ models.Project.prototype.sync = function(method, model, success, error) {
     }
 };
 
+// Returns a file stat object with additional `data` and `basename` properties.
+function read(filepath, callback) {
+    fs.stat(filepath, function(err, stat) {
+        if (err) return callback(err);
+        fs.readFile(filepath, 'utf8', function(err, data) {
+            if (err) return callback(err);
+            stat.data = data;
+            stat.basename = path.basename(filepath);
+            callback(null, stat);
+        });
+    });
+};
+
+// Returns an array of stat objects with additional `basename` property.
+function readdir(filepath, callback) {
+    fs.readdir(filepath, function(err, files) {
+        if (err) return callback(err);
+        var stats = [];
+        _(files).each(function(file) {
+            fs.stat(path.join(filepath, file), function(err, stat) {
+                if (err) return callback(err);
+                stat.basename = file;
+                stats.push(stat);
+                stats.length === files.length && callback(null, stats);
+            });
+        });
+    });
+};
+
+// https://gist.github.com/707661
+function mkdirp(p, mode, f) {
+    var cb = f || function() {};
+    if (p.charAt(0) != '/') {
+        cb('Relative path: ' + p);
+        return;
+    }
+
+    var ps = path.normalize(p).split('/');
+    path.exists(p, function(exists) {
+        if (exists) return cb(null);
+        mkdirp(ps.slice(0, -1).join('/'), mode, function(err) {
+            if (err && err.errno != process.EEXIST) return cb(err);
+            fs.mkdir(p, mode, cb);
+        });
+    });
+};
+
+// Recursive rm.
+function rm(filepath, callback) {
+    var killswitch = false;
+    fs.stat(filepath, function(err, stat) {
+        if (err) return callback(err);
+        if (stat.isFile()) return fs.unlink(filepath, callback);
+        if (!stat.isDirectory()) return callback(new Error('Unrecognized file.'));
+        Step(function() {
+            fs.readdir(filepath, this);
+        },
+        function(err, files) {
+            if (err) throw err;
+            if (files.length === 0) return this(null, []);
+            var group = this.group();
+            _(files).each(function(file) {
+                rm(path.join(filepath, file), group());
+            });
+        },
+        function(err) {
+            if (err) return callback(err);
+            fs.rmdir(filepath, callback);
+        });
+    });
+};
+
 // Load a single project model.
 function loadProject(model, callback) {
     var modelPath = path.join(settings.files, 'project', model.id);
     var object;
-    Step(
-        function() {
-            fs.stat(
-                path.join(modelPath, model.id) + '.mml',
-                this.parallel()
-            );
-            fs.readFile(
-                path.join(modelPath, model.id) + '.mml',
-                'utf-8',
-                this.parallel()
-            );
-        },
-        function(err, stat, data) {
-            if (err || !data) throw new Error('Error reading model file.');
+    Step(function() {
+        read(path.join(modelPath, model.id) + '.mml', this);
+    },
+    function(err, file) {
+        if (err) throw new Error('Error reading model file.');
 
-            // Set the object ID explicitly for multiple-load scenarios where
-            // model parse()/set() is bypassed.
-            object = JSON.parse(data);
-            object.id = model.id;
-            object._updated = + stat.mtime;
-            if (object.Stylesheet && object.Stylesheet.length > 0) {
-                var group = this.group();
-                _.each(object.Stylesheet, function(filename, index) {
-                    fs.stat(
-                        path.join(modelPath, filename),
-                        group()
-                    );
-                    fs.readFile(
-                        path.join(modelPath, filename),
-                        'utf-8',
-                        group()
-                    );
-                });
-            } else {
-                this();
-            }
-        },
-        function(err, data) {
-            if (err) return callback(err);
-
-            // Retrive the most current modified time from all stylesheets and
-            // set to project modified time if more current than project mml.
-            var mtime = _(data).chain()
-                .pluck('mtime')
-                .filter(_.isDate)
-                .map(function(date) { return + date })
-                .max()
-                .value();
-            (mtime > object._updated) && (object._updated = mtime);
-            object._updated = parseInt(object._updated);
-
-            // Unpack content of loaded Stylesheets into project object.
-            var files = _(data).chain()
-                .filter(_.isString)
-                .value();
-            if (object.Stylesheet && files) {
-                object.Stylesheet = _.reduce(
-                    object.Stylesheet,
-                    function(memo, filename, index) {
-                        if (files[index]) {
-                            memo.push({
-                                id: filename,
-                                data: files[index]
-                            });
-                        }
-                        return memo;
-                    },
-                    []
-                );
-            }
-            this(err, object);
-        },
-        function(err, object) {
-            if (err) return callback(err);
-
-            // Determine if an SRS string should be migrated.
-            var migrateMercatorSRS = function(srs) {
-                var migrationCandidate = [
-                    '+a=6378137',
-                    '+b=6378137',
-                    '+k=1.0',
-                    '+lat_ts=0.0',
-                    '+lon_0=0.0',
-                    '+nadgrids=@null',
-                    '+proj=merc',
-                    '+units=m',
-                    '+x_0=0.0',
-                    '+y_0=0'];
-
-                // Sort all proj arguments with equals signs (=) into an array.
-                var components = _(srs.split(' ')).chain()
-                    .select(function(component) {
-                        if (component.indexOf('=') !== -1) {
-                            return true;
-                        }
-                        return false;
-                    })
-                    .sortBy(function(component) { return component });
-
-                return _.isEqual(components, migrationCandidate);
-            }
-
-            // Migrate legacy srs strings for 900913.
-            var newSRS = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over';
-            // Project
-            if (migrateMercatorSRS(object.srs)) {
-                object.srs = newSRS;
-            }
-            // Layers
-            _.each(object.Layer, function(layer) {
-                if (migrateMercatorSRS(layer.srs)) {
-                    layer.srs = newSRS;
-                }
+        // Set the object ID explicitly for multiple-load scenarios where
+        // model parse()/set() is bypassed.
+        object = JSON.parse(file.data);
+        object.id = model.id;
+        object._updated = Date.parse(file.mtime);
+        if (object.Stylesheet && object.Stylesheet.length > 0) {
+            var group = this.group();
+            _(object.Stylesheet).each(function(filename) {
+                read(path.join(modelPath, filename), group());
             });
-
-            return callback(err, object);
+        } else {
+            this(null, []);
         }
-    );
+    },
+    function(err, stylesheets) {
+        if (err) throw err;
+
+        // Retrive the most current modified time from all stylesheets and
+        // set to project modified time if more current than project mml.
+        var mtime = _(stylesheets).chain()
+            .pluck('mtime')
+            .map(function(mtime) { return Date.parse(mtime); })
+            .max()
+            .value();
+        (mtime > object._updated) && (object._updated = mtime);
+
+        object.Stylesheet = _(stylesheets).map(function(file) {
+            return { id: file.basename, data: file.data };
+        });
+        this();
+    },
+    function(err) {
+        if (err) throw err;
+
+        // Determine if an SRS string should be migrated.
+        var migrateSRS = function(srs) {
+            var migrationCandidate = [
+                '+a=6378137',
+                '+b=6378137',
+                '+k=1.0',
+                '+lat_ts=0.0',
+                '+lon_0=0.0',
+                '+nadgrids=@null',
+                '+proj=merc',
+                '+units=m',
+                '+x_0=0.0',
+                '+y_0=0'];
+            // Sort all proj arguments with equals signs (=) into an array.
+            var components = _(srs.split(' ')).chain()
+                .select(function(component) {
+                    return (component.indexOf('=') !== -1);
+                })
+                .sortBy(function(component) { return component });
+            return _.isEqual(components, migrationCandidate);
+        }
+        // Migrate legacy srs strings for 900913.
+        var newSRS = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over';
+        // Migrate project, layer SRS strings.
+        var queue = [];
+        object.srs && queue.push(object);
+        object.Layer && (queue = queue.concat(object.Layer));
+        _(queue).each(function(obj) {
+            obj.srs = obj.srs && migrateSRS(obj.srs) && newSRS;
+        });
+        this();
+    },
+    function(err) {
+        return callback(err, object);
+    });
 };
 
 // Load all projects into an array.
 function loadProjectAll(model, callback) {
     var basepath = path.join(settings.files, 'project');
-    Step(
-        function() {
-            path.exists(basepath, this);
-        },
-        function(exists) {
-            if (!exists) {
-                fs.mkdir(basepath, 0777, this);
-            } else {
-                this();
-            }
-        },
-        function() {
-            fs.readdir(basepath, this);
-        },
-        function(err, files) {
-            if (err) {
-                return this('Error reading model directory.');
-            }
-            else if (files.length === 0) {
-                return this();
-            }
-            var group = this.group();
-            for (var i = 0; i < files.length; i++) {
-                var id = files[i];
-                loadProject({ id: id }, group());
-            }
-        },
-        function(err, models) {
-            // Ignore errors from loading individual models (e.g.
-            // don't let one bad apple spoil the collection).
-            models = _.select(models, function(model) {
-                return (typeof model === 'object');
-            });
-            return callback(null, models);
-        }
-    );
+    Step(function() {
+        mkdirp(basepath, 0777, this);
+    },
+    function(err) {
+        if (err) throw err;
+        readdir(basepath, this);
+    },
+    function(err, files) {
+        if (err) throw new Error('Error reading model directory.');
+        if (files.length === 0) return this(null, []);
+        var group = this.group();
+        _(files).chain()
+            .filter(function(file) { return file.isDirectory() })
+            .each(function(file) { loadProject({id:file.basename}, group()) });
+    },
+    function(err, models) {
+        // Ignore errors from loading individual models (e.g.
+        // don't let one bad apple spoil the collection).
+        models = _(models).select(function(model) {
+            return model && model.id
+        });
+        return callback(null, models);
+    });
 };
 
 // Destroy a project. `rm -rf` equivalent for the project directory.
 function destroyProject(model, callback) {
-    var rm = function(basePath, callback) {
-        var killswitch = false;
-        Step(
-            function() {
-                fs.stat(basePath, this);
-            },
-            function(err, stat) {
-                if (stat.isDirectory()) {
-                    this();
-                } else if (stat.isFile()) {
-                    killswitch = true;
-                    fs.unlink(basePath, this);
-                } else {
-                    killswitch = true;
-                    this();
-                }
-            },
-            // The next steps apply only when basePath refers to a directory.
-            function(err) {
-                if (killswitch) return this();
-                fs.readdir(basePath, this);
-            },
-            function(err, files) {
-                if (killswitch) return this();
-                if (files.length === 0) {
-                    this();
-                } else {
-                    var group = this.group();
-                    for (var i = 0; i < files.length; i++) {
-                        rm(path.join(basePath, files[i]), group());
-                    }
-                }
-            },
-            function(err) {
-                if (killswitch) return callback();
-                fs.rmdir(basePath, callback);
-            }
-        );
-    };
     var modelPath = path.join(settings.files, 'project', model.id);
     rm(modelPath, callback);
 }
@@ -261,100 +233,66 @@ function destroyProject(model, callback) {
 // Save a project. Creates a subdirectory per project and splits out
 // stylesheets into separate files.
 function saveProject(model, callback) {
-    var basePath = path.join(settings.files, 'project');
     var modelPath = path.join(settings.files, 'project', model.id);
-    Step(
-        function() {
-            path.exists(basePath, this);
-        },
-        function(exists) {
-            if (!exists) {
-                fs.mkdir(basePath, 0777, this);
-            } else {
-                this();
-            }
-        },
-        function() {
-            path.exists(modelPath, this);
-        },
-        function(exists) {
-            if (!exists) {
-                fs.mkdir(modelPath, 0777, this);
-            } else {
-                this();
-            }
-        },
-        function() {
-            fs.readdir(modelPath, this);
-        },
-        function(err, files) {
-            // Remove any stale files in the project directory.
-            if (err) throw err;
-            var group = this.group();
-            var stylesheets = model.get('Stylesheet') || [];
-            var stale = _(files).select(function(filename) {
-                if (filename[0] === '.') {
-                    return false;
-                } else if (filename === (model.id + '.mml')) {
-                    return false;
-                } else if (_(stylesheets).pluck('id').indexOf(filename) !== -1) {
-                    return false;
-                }
-                return true;
-            });
-            for (var i = 0; i < stale.length; i++) {
-                var next = group();
-                var filepath = path.join(modelPath, stale[i]);
-                fs.stat(filepath, function(err, stat) {
-                    if (err) return next(err);
-                    if (stat.isFile()) return fs.unlink(filepath, next);
-                    return next();
-                });
-            }
-            group()();
-        },
-        function(err) {
-            if (err) throw err;
-            // Hard clone the model JSON before doing adjustments to the data
-            // based on writing separate stylesheets.
-            var data = JSON.parse(JSON.stringify(model.toJSON()));
-            var files = [];
-            if (data.id) delete data.id;
-            if (data._updated) delete data._updated;
-            if (data.Stylesheet) {
-                _.each(data.Stylesheet, function(stylesheet, key) {
-                    if (stylesheet.id) {
-                        files.push({
-                            filename: stylesheet.id,
-                            data: stylesheet.data
-                        });
-                        data.Stylesheet[key] = stylesheet.id;
-                    }
-                });
-            }
-            files.push({
-                filename: model.id + '.mml',
-                data: JSON.stringify(data, null, 2)
-            });
+    Step(function() {
+        mkdirp(modelPath, 0777, this);
+    },
+    function(err) {
+        if (err) throw err;
+        readdir(modelPath, this);
+    },
+    function(err, files) {
+        if (err) throw err;
 
-            var group = this.group();
-            for (var i = 0; i < files.length; i++) {
-                fs.writeFile(
-                    path.join(modelPath, files[i].filename),
-                    files[i].data,
-                    group()
-                );
-            }
-        },
-        function() {
-            fs.stat(path.join(modelPath, model.id + '.mml'), this);
-        },
-        function(err, stat) {
-            var attr = {};
-            stat && (attr._updated = Date.parse(stat.mtime));
-            callback(null, attr);
+        // Remove any stale files in the project directory.
+        var stylesheets = _(model.get('Stylesheet')).pluck('id') || [];
+        var group = this.group();
+        _(files).chain()
+            .select(function(file) {
+                if (!file.isFile()) return false;
+                if (file.basename[0] === '.') return false;
+                if (file.basename === model.id + '.mml') return false;
+                if (_(stylesheets).include(file.basename)) return false;
+                return true;
+            })
+            .each(function(file) {
+                fs.unlink(path.join(modelPath, file.basename), group());
+            });
+        group()();
+    },
+    function(err) {
+        if (err) throw err;
+        // Hard clone the model JSON before doing adjustments to the data
+        // based on writing separate stylesheets.
+        var data = JSON.parse(JSON.stringify(model.toJSON()));
+        var files = [];
+        if (data.id) delete data.id;
+        if (data._updated) delete data._updated;
+        if (data.Stylesheet) {
+            data.Stylesheet = _(data.Stylesheet).map(function(s) {
+                s.id && files.push(s);
+                return s.id || s;
+            });
         }
-    );
+        files.push({
+            id: model.id + '.mml',
+            data: JSON.stringify(data, null, 2)
+        });
+
+        var group = this.group();
+        _(files).each(function(file) {
+            fs.writeFile(path.join(modelPath, file.id), file.data, group());
+        });
+    },
+    function(err) {
+        if (err) throw err;
+        fs.stat(path.join(modelPath, model.id + '.mml'), this);
+    },
+    function(err, stat) {
+        var attr = {};
+        stat && (attr._updated = Date.parse(stat.mtime));
+        callback(err, attr);
+    });
 }
 
 // Export
