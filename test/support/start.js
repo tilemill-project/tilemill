ddb2ebd735be4d1a216c56685ef53e70324bfa0d
Konstantin Käfer <github@kkaefer.com>
2011-07-26 20:12:31 +0200

    first new tests

---
+++ test/support/start.js
@@ -0,0 +1,25 @@
+// process.env.NODE_ENV = 'test';
+process.argv[2] = 'start';
+
+var exec = require('child_process').exec;
+
+// Load application. This file's purpose is to start TileMill only once and
+// share the server with all tests.
+require('../..');
+var tilemill = require('bones').plugin;
+
+tilemill.config.files = './test/fixtures/files';
+
+var started = false, waiting = [];
+var command = tilemill.start(function() {
+    exec('./test/support/cleanup.sh', function(err) {
+        if (err) throw err;
+        started = true;
+        for (var fn; fn = waiting.shift();) fn(command);
+    });
+});
+
+module.exports = function(cb) {
+    if (started) cb(command);
+    else waiting.push(cb);
+};
