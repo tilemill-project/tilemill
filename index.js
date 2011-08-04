220470d3721012b8b3fd6042cf652eefef735cf4
Konstantin Käfer <github@kkaefer.com>
2011-08-04 15:01:07 -0400

    fix exporting in the os x app while maintaining compatibility with the ubuntu node ppa

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 process.title = 'tilemill';
 
+// This is necessary to make optimist not special-case into coffeescript as
+// certain node installs (e.g. ubuntu node ppa) do not use `node` as the binary
+// name.
+process.argv[0] = 'node';
+
 require('tilelive-mapnik').registerProtocols(require('tilelive'));
 require('mbtiles').registerProtocols(require('tilelive'));
 
