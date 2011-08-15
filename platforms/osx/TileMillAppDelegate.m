d9647079dc6b2113526f2a233735976563044869
Justin R. Miller <incanus@codesorcery.net>
2011-08-15 18:31:07 -0400

    updated copyright headers where missing

--- platforms/osx/TileMillAppDelegate.m
+++ platforms/osx/TileMillAppDelegate.m
@@ -1,3 +1,10 @@
+//
+//  TileMillAppDelegate.m
+//  tilemill
+//
+//  Created by Dane Springmeyer on 7/28/11.
+//  Copyright 2011 Development Seed. All rights reserved.
+//
 
 #import "TileMillAppDelegate.h"
 #import "TileMillMainWindowController.h"
