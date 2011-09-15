fc1e1b792e1ebea26a836c35e662c5b2d8cce14d
Will White <will@developmentseed.org>
2011-09-15 11:11:55 -0400

    Remove kill, pass on args. Refs #552.

--- platforms/osx/tilemill.sh
+++ platforms/osx/tilemill.sh
@@ -1,27 +1,11 @@
 #!/bin/sh
 
-#  tilemill.sh
-#  TileMill
-#
-#  Created by Justin Miller on 9/14/11.
-#  Copyright 2011 Development Seed. All rights reserved.
-#
 #  This shell script is designed to be linked to someplace
-#  like $HOME/bin so that TileMill can be easily invoked 
-#  from the command line. 
-
-#  Kill existing node process.
-#
-kill $( ps ax | grep -i TileMill.app | grep index.js | sed -n 1p | awk '{ print $1 }' ) 2>/dev/null
-
-#  Kill existing Mac app.
-#
-kill $( ps ax | grep -i TileMill.app | grep MacOS    | sed -n 1p | awk '{ print $1 }' ) 2>/dev/null
+#  like $HOME/bin so that TileMill can be easily invoked
+#  from the command line.
 
 #  Change to bundle directory.
-#
 cd `dirname $( readlink $0 )`
 
 #  Run manually.
-#
-./index.js
+./index.js $@
