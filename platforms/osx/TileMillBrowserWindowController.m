9c3b5004b5c509dc9f109ea57f3ddb81236dd71c
Justin R. Miller <incanus@codesorcery.net>
2011-10-26 09:15:36 -0700

    only need to adjust scroll bounce the first time the web view loads

--- platforms/osx/TileMillBrowserWindowController.m
+++ platforms/osx/TileMillBrowserWindowController.m
@@ -64,24 +64,29 @@
 - (void)webView:(WebView *)sender didCommitLoadForFrame:(WebFrame *)frame
 {
     if ( ! self.initialRequestComplete)
-        self.initialRequestComplete = YES;
-
-    /*
-     * This is a nasty runtime hack to get rid of scroll bouncing when
-     * building the app against the 10.6 SDK. We'll get around this soon
-     * by building on 10.7.
-     */
-    
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.25 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void)
     {
-        NSScrollView *scroller = self.webView.mainFrame.frameView.documentView.enclosingScrollView;
+        self.initialRequestComplete = YES;
         
-        if ([scroller respondsToSelector:@selector(setHorizontalScrollElasticity:)])
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.25 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void)
         {
-            objc_msgSend(scroller, @selector(setHorizontalScrollElasticity:), 1);
-            objc_msgSend(scroller, @selector(setVerticalScrollElasticity:),   1);
-        }
-    });
+            /*
+             * This is a nasty runtime hack to get rid of scroll bouncing when
+             * building the app against the 10.6 SDK. We'll get around this soon
+             * by building on 10.7.
+             *
+             * We do this on a delay so that the first request has time to start
+             * rendering on the page. Otherwise, the scrollbars don't exist yet.
+             */
+
+            NSScrollView *scroller = self.webView.mainFrame.frameView.documentView.enclosingScrollView;
+           
+            if ([scroller respondsToSelector:@selector(setHorizontalScrollElasticity:)])
+            {
+                objc_msgSend(scroller, @selector(setHorizontalScrollElasticity:), 1);
+                objc_msgSend(scroller, @selector(setVerticalScrollElasticity:),   1);
+            }
+        });
+    }
 }
 
 #pragma mark -
