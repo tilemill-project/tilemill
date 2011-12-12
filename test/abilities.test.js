31b76f71023572ab1f42af528536bb015cb8c074
Young Hahn <young@developmentseed.org>
2011-12-12 13:14:20 -0500

    Test for version strings in abilities.

--- test/abilities.test.js
+++ test/abilities.test.js
@@ -9,6 +9,8 @@ require('./support/start')(function(command) {
                 var body = res.body.replace(/^\s*var\s+abilities\s*=\s*(.+?);?$/, '$1');
                 var abilities = JSON.parse(body);
 
+                assert.ok(/v\d+.\d+.\d+-\d+-[a-z0-9]+/.test(abilities.version[0]));
+                assert.ok(/\d+.\d+.\d+.\d+/.test(abilities.version[1]));
                 assert.ok(abilities.fonts.indexOf('Arial Regular') >= 0 ||
                           abilities.fonts.indexOf('DejaVu Sans Book') >= 0);
                 assert.deepEqual([0,206,209], abilities.carto.colors.darkturquoise);
