b189e61fb4a9d3813d4cde99dac0a78e3556a1de
Dane Springmeyer <dane@dbsgeo.com>
2012-01-25 20:27:43 -0800

    windows: run index.js from current directory

--- platforms/windows/run-tilemill.bat
+++ platforms/windows/run-tilemill.bat
@@ -6,4 +6,4 @@ cd ..\..\
 set PROJ_LIB=data\proj\nad
 set GDAL_DATA=data\gdal\data
 set PATH=node_modules\mapnik\lib\mapnik\lib;node_modules\zipfile\lib;%PATH%
-start /min cmd /C "node index.js 1>>"%USERPROFILE%"\tilemill.log 2>&1"
+start /min cmd /C "node .\index.js 1>>"%USERPROFILE%"\tilemill.log 2>&1"
