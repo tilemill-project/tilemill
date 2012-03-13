305c5cd44c55e7e644be03be1d64c7045d29074d
Dane Springmeyer <dane@dbsgeo.com>
2012-03-13 16:21:40 -0700

    win: put c++ modules into addons folder again to make it easy to clear our node_modules

--- platforms/windows/run-tilemill.bat
+++ platforms/windows/run-tilemill.bat
@@ -5,5 +5,7 @@ cd /d %~dp0
 cd ..\..\
 set PROJ_LIB=data\proj\nad
 set GDAL_DATA=data\gdal\data
-set PATH=node_modules\mapnik\lib\mapnik\lib;node_modules\zipfile\lib;%PATH%
-start /min cmd /C "node .\index.js 1>>"%USERPROFILE%"\tilemill.log 2>&1"
+set PATH=addons\mapnik\lib\mapnik\lib;addons\zipfile\lib;%PATH%
+set NODE_PATH=addons;%NODE_PATH%
+@rem start /min cmd /C "node .\index.js 1>>"%USERPROFILE%"\tilemill.log 2>&1"
+node.exe .\index.js
