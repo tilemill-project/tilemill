tilemill-project/tilemill

commit e0f82b669dab06efcccc237f2363b8619c1c6e55

explicitly declare line placement and arrow type

examples/open-streets-dc/highway.mss
@@ -436,7 +436,8 @@
 /* ---- ONE WAY ARROWS ---- */
 
 .highway.fill::oneway_arrow[zoom>15][ONEWAY='yes'] {
-  marker-type:arrow;
+  marker-file:"shape://arrow";
+  marker-placement:line;
   marker-line-width:1;
   marker-line-opacity:0.5;
   marker-line-color:#fff;

commit 8733740c343771088f005f1f2d658479fd814f79

fix open-streets-dc example syntax that I accidentally broke in e0f82b669dab06efcccc237f2363b8619c1c6e55 - closes #1639

examples/open-streets-dc/highway.mss
@@ -436,7 +436,8 @@
 /* ---- ONE WAY ARROWS ---- */
 
 .highway.fill::oneway_arrow[zoom>15][ONEWAY='yes'] {
-  marker-file:"shape://arrow";
+  marker-file:url("shape://arrow");
+  marker-width:15;
   marker-placement:line;
   marker-line-width:1;
   marker-line-opacity:0.5;