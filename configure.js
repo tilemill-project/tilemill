956537aa2010a5d95d31688801d74fa2c7ebe2c4
Dane Springmeyer <dane@dbsgeo.com>
2013-10-04 15:49:03 -0700

    add protobuf/pkg-config to configure script

--- configure.js
+++ configure.js
@@ -5,22 +5,43 @@ var cp = require('child_process');
 var prefix = '[tilemill configure] ';
 
 function mapnik_config(callback) {
-	cp.execFile('mapnik-config', ['--version'], function(err, stdout, stderr) {
+    cp.execFile('mapnik-config', ['--version'], function(err, stdout, stderr) {
         if (err || stderr) {
-        	return callback(err || new Error(stderr.trim()));
+            return callback(err || new Error(stderr.trim()));
         } else {
-        	return callback(null,stdout.trim());
+            return callback(null,stdout.trim());
         }
     });
 }
 
+function protobuf_config(callback) {
+    if (process.platform === 'win32') {
+        return callback(null,'skipped protobuf check');
+    } else {
+        cp.execFile('pkg-config', ['protobuf','--version'], function(err, stdout, stderr) {
+            if (err || stderr) {
+                return callback(err || new Error(stderr.trim()));
+            } else {
+                return callback(null,stdout.trim());
+            }
+        });
+    }
+}
+
 mapnik_config(function(err,result) {
-	if(err) {
-		console.error(prefix + 'Could not configure TileMill because the "mapnik-config" program was not found (' + err.message + ')');
-		process.exit(1);
-	} else {
-		console.log(prefix + 'Successfully found mapnik version v'+result + ' (via mapnik-config)');
-		console.log(prefix + 'Using node v'+ process.versions.node);
-		process.exit(0);
-	}
+    console.log(prefix + 'Using node v'+ process.versions.node);
+    if(err) {
+        console.error(prefix + 'Could not configure TileMill because the "mapnik-config" program was not found (' + err.message + ')');
+        process.exit(1);
+    } else {
+        console.log(prefix + 'Successfully found mapnik version v'+result + ' (via mapnik-config)');
+        protobuf_config(function(err,result) {
+            if (err) {
+                console.error(prefix + 'Could not configure TileMill because "pkg-config protobuf --version" returned an error (' + err.message + ')');
+            } else {
+                console.log(prefix + 'Found libprotobuf v'+result)
+                process.exit(0);
+            }
+        });
+    }
 });
