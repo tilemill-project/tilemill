eb9939958ab39546d39973dbff50ce6aad780f76
Dane Springmeyer <dane@dbsgeo.com>
2013-10-18 12:31:39 -0400

    tests: adapt to smaller default image size: #1764

--- test/tile.test.js
+++ test/tile.test.js
@@ -30,7 +30,7 @@ it ('should 200 (tile) for existing project', function(done) {
         { url: '/tile/demo_01/2/2/1.png', encoding: 'binary' },
         { status: 200 },
         function(res) {
-            assert.equal(res.body.length, 17879);
+            assert.equal(res.body.length, 7986);
             done();
         }
     );
