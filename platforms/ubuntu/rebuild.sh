f7adadcd489d23119257948346d774a5cd4578dd
Dane Springmeyer <dane@dbsgeo.com>
2013-10-25 11:16:32 -0700

    move rebuild script for cxx modules to root since platforms directory is thrown out in ubuntu packaging

--- platforms/ubuntu/rebuild.sh
+++
@@ -1,8 +0,0 @@
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${CURRENT_DIR}/../../
-TILEMILL_ROOT=`pwd`
-PATH=${TILEMILL_ROOT}/node_modules/.bin:${PATH}
-BUILD_ROOT=${TILEMILL_ROOT}/node_root_dir
-CXX_MODULES=$(find ${TILEMILL_ROOT}/node_modules -name binding.gyp | sed 's,/binding.gyp,/,g')
-for i in ${CXX_MODULES}; do cd $i && node-gyp rebuild --nodedir=${BUILD_ROOT}; done
-
