263328c013f147b9ed79dc7b87f4649a1ad557cd
Dane Springmeyer <dane@dbsgeo.com>
2013-11-18 17:30:28 -0800

    move scripts to copy tilemill node files out of xcode project

---
+++ platforms/osx/copy_node.sh
@@ -0,0 +1,11 @@
+if [ -z $( which node ) ]; then
+  echo "Unable to find node binary"
+  exit 1
+fi
+
+TARGET="$1/"
+# copy node executable
+cp `which node` $TARGET
+# change shebang to use local node
+sed -i.backup 's/#!\/usr\/bin\/env node/#!node/' ${TARGET}index.js
+rm ${TARGET}index.js.backup
