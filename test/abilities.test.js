dc7ea9be3865526399bfb3df4c4443f1dfe25b1d
Dane Springmeyer <dane@dbsgeo.com>
2013-12-18 23:10:06 -0500

    output version when abilities test for version fails

--- test/abilities.test.js
+++ test/abilities.test.js
@@ -26,8 +26,8 @@ it('GET should return JSON', function(done) {
             var body = res.body.replace(/^\s*var\s+abilities\s*=\s*(.+?);?$/, '$1');
             var abilities = JSON.parse(body);
 
-            assert.ok(/v\d+.\d+.\d+-\d+-[a-z0-9]+/.test(abilities.version[0]));
-            assert.ok(/\d+.\d+.\d+.\d+/.test(abilities.version[1]));
+            assert.ok(/v\d+.\d+.\d+-\d+-[a-z0-9]+/.test(abilities.version[0]),abilities.version[0]);
+            assert.ok(/\d+.\d+.\d+.\d+/.test(abilities.version[1]),abilities.version[1]);
             assert.ok(abilities.fonts.indexOf('Arial Regular') >= 0 ||
                       abilities.fonts.indexOf('DejaVu Sans Book') >= 0);
             assert.deepEqual([0,206,209], abilities.carto.colors.darkturquoise);
