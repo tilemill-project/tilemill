f3e9960c1c737888bee9dd0316b9bb6c02663a05
Dane Springmeyer <dane@dbsgeo.com>
2014-01-09 14:21:42 -0800

    improve the error output of the duplicate module test in the case that modules are missing

--- test/duplicate_module.test.js
+++ test/duplicate_module.test.js
@@ -5,7 +5,12 @@ var count_module = function(name,callback) {
     var cmd = 'npm ls ' + name;
     exec(cmd,
         function (error, stdout, stderr) {
-        return callback(null,stdout.match(new RegExp(name+'@','g')).length);
+            var pattern = new RegExp(name+'@','g');
+            var match = stdout.match(pattern);
+            if (!match) {
+                return callback(null,0);
+            }
+            return callback(null,match.length);
     });
 };
 
@@ -15,7 +20,8 @@ describe('config loading pwnage', function() {
     it('there should only be one ' + mod + ' module otherwise you are hosed', function(done) {
          count_module(mod, function(err,count) {
             if (err) throw err;
-            assert.equal(count,1,'you have more than one copy of ' + mod + ' (based on npm ls output)')
+            assert.notEqual(count,0,'you are missing the ' + mod + ' module (`npm ls ' + mod + '`)');
+            assert.equal(count,1,'you have more than one copy of ' + mod + ' (`npm ls ' + mod + '`)');
             done();
         });
     });
