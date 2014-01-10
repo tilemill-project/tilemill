63bfd18837ff6f59f0d5317b21e19c14b9c213a6
Dane Springmeyer <dane@dbsgeo.com>
2014-01-09 16:02:28 -0800

    skip strict version tests on travis since git shallow clone prevents tag being known (which is okay)

--- test/abilities.test.js
+++ test/abilities.test.js
@@ -25,9 +25,12 @@ it('GET should return JSON', function(done) {
         function(res) {
             var body = res.body.replace(/^\s*var\s+abilities\s*=\s*(.+?);?$/, '$1');
             var abilities = JSON.parse(body);
-
-            assert.ok(/v\d+.\d+.\d+-\d+-[a-z0-9]+/.test(abilities.version[0]),abilities.version[0]);
-            assert.ok(/\d+.\d+.\d+.\d+/.test(abilities.version[1]),abilities.version[1]);
+            // travis does a shallow clone so the git version and hash will
+            // not be known, which is harmless
+            if (!process.env.TRAVIS) {
+                assert.ok(/v\d+.\d+.\d+-\d+-[a-z0-9]+/.test(abilities.version[0]),abilities.version[0]);
+                assert.ok(/\d+.\d+.\d+.\d+/.test(abilities.version[1]),abilities.version[1]);
+            }
             assert.ok(abilities.fonts.indexOf('Arial Regular') >= 0 ||
                       abilities.fonts.indexOf('DejaVu Sans Book') >= 0);
             assert.deepEqual([0,206,209], abilities.carto.colors.darkturquoise);
