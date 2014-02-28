1c3e5ee5980a1f5acdf72fee0a56fa8fbff8ecd2
Justin R. Miller <incanus@codesorcery.net>
2014-02-28 11:58:04 -0800

    fixes #2224: rename node executable for OS X firewall prompts

--- platforms/osx/copy_node.sh
+++ platforms/osx/copy_node.sh
@@ -6,7 +6,7 @@ fi
 TARGET="$1/"
 # copy node executable
 rm -f $TARGET/node
-cp `which node` $TARGET
-# change shebang to use local node
-sed -i.backup 's/#!\/usr\/bin\/env node/#!\.\/node/' ${TARGET}index.js
+cp `which node` $TARGET/TileMill # for OS X firewall dialogs
+# change shebang to use local aliases node
+sed -i.backup 's/#!\/usr\/bin\/env node/#!\.\/TileMill/' ${TARGET}index.js
 rm ${TARGET}index.js.backup
