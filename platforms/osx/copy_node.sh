6baef03e1637ac121d3031f793940384403555cc
Justin R. Miller <incanus@codesorcery.net>
2014-02-28 11:23:14 -0800

    remove old OS X node first to avoid perm problems

--- platforms/osx/copy_node.sh
+++ platforms/osx/copy_node.sh
@@ -5,6 +5,7 @@ fi
 
 TARGET="$1/"
 # copy node executable
+rm -f $TARGET/node
 cp `which node` $TARGET
 # change shebang to use local node
 sed -i.backup 's/#!\/usr\/bin\/env node/#!\.\/node/' ${TARGET}index.js
