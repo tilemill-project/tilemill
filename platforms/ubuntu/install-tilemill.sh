bd21d6926e16c894fe4ec13a786f657afb894ad0
Dane Springmeyer <dane@mapbox.com>
2014-06-16 12:52:04 -0700

    fix #2354

--- platforms/ubuntu/install-tilemill.sh
+++ platforms/ubuntu/install-tilemill.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-### IMPORTANT - If you already have a version of mapnik installed, such as
-### a version you compiled while installing a previous version of TileMill
-### you must uninstall that version of mapnik in order to be able to use
-### the packaged version of TileMill.  To uninstall, for example, go to
-### the directory from where you installed mapnik, such as 
-### /home/ubuntu/mapnik and from within that directory run
-### `sudo make uninstall` to uninstall that version of mapnik.
-### If you install the packaged version of TileMill using this script and
-### then you see segmentation fault errors in the TileMill log in
-### /var/log/tilemill/tilemill.log, then chances are you have a previously
-### compiled version of mapnik, in addition to the mapnik package required
-### and which will be installed for you using this script.
-
 pkexec /bin/bash -c "apt-get install --yes python-software-properties &&
-    yes | apt-add-repository ppa:developmentseed/mapbox &&
+    apt-add-repository --yes ppa:developmentseed/mapbox &&
     apt-get update --yes &&
     apt-get install --yes tilemill"
 
