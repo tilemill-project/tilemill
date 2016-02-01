5b8cfbb6cf0b77f1c4acedd6d515453b3695448d
florianf <florian.falkner@general-solutions.at>
2016-02-01 20:46:05 +0100

    Removed native platform builds directory.

--- platforms/ubuntu/install-tilemill.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pkexec /bin/bash -c "apt-get install --yes python-software-properties &&
-    apt-add-repository --yes ppa:developmentseed/mapbox &&
-    apt-get update --yes &&
-    apt-get install --yes tilemill"
-
-read -sp "Press [ENTER] to quit."
