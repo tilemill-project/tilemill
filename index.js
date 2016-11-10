35fbcd9588a0e642766f086db7fb81f564052f67
florianf <florian.falkner@general-solutions.at>
2016-11-10 23:22:26 +0100

    node-zip is no more

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ if (process.platform === 'win32') {
     // HOME is undefined on windows
     process.env.HOME = process.env.USERPROFILE;
     // Add custom library paths to the PATH
-    process.env.PATH = path.join(__dirname,"node_modules/mapnik/lib/binding/") +
-        ";" + path.join(__dirname,"node_modules/zipfile/lib");
+    process.env.PATH = path.join(__dirname,"node_modules/mapnik/lib/binding/");
 }
 
 // Default --config flag to user's home .tilemill.json config file.
