56aec35c97ce4526a9fe5b1782b356a3571148f9
csytsma <sijitsma@gmail.com>
2019-02-03 12:35:02 -0800

    Revert "Fixed text for invalid stylesheet"
    This reverts commit 894cdf3f0b815b1c93ff31c029bf8fa2e1ba9033.

--- models/Project.server.bones
+++ models/Project.server.bones
@@ -472,14 +472,9 @@ function compileStylesheet(mml, callback) {
     };
 
     // try/catch here as per https://github.com/mapbox/tilemill/issues/1370
-    // with carto 1.x, it no longer throws an error for compile errors, instead data is null
     try {
         var xml = new carto.Renderer(env, { mapnik_version: mapnik.versions.mapnik }).render(mml);
-        if (xml.data != null) {
-            return callback(null, xml);
-        } else {
-            return callback(new Error(xml.msg[0].message));
-        }
+        return callback(null, xml);
     } catch (err) {
         return callback(err);
     }
--- test/project.test.js
+++ test/project.test.js
@@ -185,9 +185,7 @@ it('PUT should fail with invalid stylesheet', function(done) {
         var body = JSON.parse(res.body);
         delete body.stack;
         assert.deepEqual({
-            // Carto 1.x isn't returning the same formatted error message anymore
-            //message: "style.mss:2:2 Invalid value for background-color, the type color is expected. blurb (of type keyword) was given."
-            message: "Invalid value for background-color, the type color is expected. blurb (of type keyword) was given."
+            message: "style.mss:2:2 Invalid value for background-color, the type color is expected. blurb (of type keyword) was given."
         }, body);
         done();
     });
