cb56ccb699bd06ebe966de6c1eb7af33d3455b32
csytsma <sijitsma@gmail.com>
2019-02-18 17:49:49 -0800

    Updated runclient.sh to check if port is open
    Instead of waiting for 12 seconds, check if the localhost:20009 port is available, before proceeding to open browser.

--- utils/runclient.sh
+++ utils/runclient.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
-sleep 12 # Give some time for the server to startup.
-open "http://localhost:20009"
+# Check if TileMill port is open...
+host="127.0.0.1"
+port="20009"
+port_status="closed"
+timeout=15
+counter=0
+
+while [ $port_status != "open" ]
+do
+ sleep 1
+ (( counter += 1 ))
+ #echo "Checking if ${host}:${port} is open..."
+ port_status=`(echo >/dev/tcp/$host/$port) &>/dev/null && echo "open" || echo "close"`
+ if [ $counter == $timeout ]; then
+   echo "TileMill is not available at ${host}:${port}";exit 1;
+ fi
+done
+echo "Opening TileMill session in browser..."
+open "http://${host}:${port}"
 exit 0
