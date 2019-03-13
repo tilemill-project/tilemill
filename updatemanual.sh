58a7735553a533dd200eee27a83585166b5868ca
Tim Potter <tim.potter@mail.com>
2019-03-12 18:29:00 -0700

    Changed updatemanual to not do the git add. Fixed the name of udpatemanual.sh in CONTRIBUTING.

--- updatemanual.sh
+++ updatemanual.sh
@@ -5,8 +5,6 @@ TILEMILL_SOURCES="${HOME}/tilemill"
 rm -rf ${TILEMILL_SOURCES}/assets/manual
 mkdir -p ${TILEMILL_SOURCES}/assets/manual
 cp -r ${TILEMILL_GHPAGES}/assets/manual/* ${TILEMILL_SOURCES}/assets/manual/
-git add ${TILEMILL_SOURCES}/assets/manual/*
 rm -rf ${TILEMILL_SOURCES}/_posts/docs/reference
 mkdir -p ${TILEMILL_SOURCES}/_posts/docs/reference
 cp -r ${TILEMILL_GHPAGES}/_posts/docs/reference/* ${TILEMILL_SOURCES}/_posts/docs/reference/
-git add ${TILEMILL_SOURCES}/_posts/docs/reference/*
