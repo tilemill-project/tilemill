0818f25afa00a276e804684f82e5a9e55c123049
Tim Potter <46423212+tpotter7@users.noreply.github.com>
2019-06-21 20:55:45 -0700

    Updates to install/load scripts (#2725)
    * updating to support remote DB.
    * updated scripts to work on linux as well as osx and also added uninstall options.
    * Fixed a small bug on osx.
    * added auto-yes to apt-get.
    * replaced hard coded user names with variables.
    * A few fixes to get it fully working on linux.
    * Fixing some issues that are showing up on osx now with these scripts.
    * Making install not error out even if already installed.

--- utils/installdb.sh
+++ utils/installdb.sh
@@ -1,97 +1,280 @@
 #!/bin/bash
 
+# Determine OS.
+info="$(tput setaf 6)";warning="$(tput setaf 3)Warning: ";error="$(tput setaf 1)Error: ";success="$(tput setaf 2)";reset=$(tput sgr0) # Colors for text
+case "${OSTYPE}" in
+  darwin*)
+    OS="osx";;
+  linux*)
+    OS="linux";;
+  solaris*)
+    echo "${warning}solaris OS detected, trying linux solution which may or may not work.${reset}";
+    OS="linux";;
+  bsd*)
+    echo "${warning}bsd OS detected, trying linux solution which may or may not work.${reset}";
+    OS="linux";;
+  msys*)
+    echo "${error}windows OS detected, to use this script on a windows machine, try running it in ubuntu on WSL.${reset}"; print_usage; exit 1;;
+  *)
+    echo "${warning}unknown OS detected, trying linux solution which may or may not work.${reset}";
+    OS="linux";;
+esac
+
 # Initialize global variables.
 POSTGIS_DB="template_postgis"           # PostGIS template DB (used to create other DBs).
 OSM_DB="osm"                            # OSM DB (used to hold OSM data).
 DB_USERNAME=$(whoami)                   # Postgres usernam (default is your Mac login).
-info=`tput setaf 6`;error=`tput setaf 1`;success=`tput setaf 2`;reset=`tput sgr0` # Colors for text
+DB_USERDEF="postgres"                   # Default DB username on Linux installations.
 
 print_intro () {
-  echo "This script will install a Postgres database which is from which TileMill will get OSM data that you download and load into the database using the osmload.sh script. The script will also install the Postgis extension that is required by the database and the osm2pgsql database loading tool. This script is only meant to be used when these tools have not already been loaded onto your Mac."
+  echo "This script will install a local Postgres database which is from which TileMill will get OSM data that you download and load into the database using the osmload.sh script. The script will also install the Postgis extension that is required by the database and the osm2pgsql database loading tool. This script is only meant to be used when these tools have not already been loaded onto your Mac."
 }
 
 print_usage () {
   echo "Usage:"
   echo "    $0                  Install the database and tools."
   echo "    $0 -h               Print help."
+  echo "    $0 -u               Uninstall."
+}
+
+# Uninstall a previous installation that was done using this script.
+uninstall () {
+  echo ""
+  echo "${warning}This function is only meant to uninstall components that were previously installed by this script. This will include uninstalling postgres, libpng, postgis, osm2pgsql, and any other packages that are unused at the end of this process. If you do not want any of these components uninstalled from your system, you should abort.${reset}"
+  echo "Continue with uninstall?[y/n]:";
+  read answer
+  if [ ${answer} != "y" ]; then
+      echo "${error}Aborting.${reset}"; exit 1
+  fi
+      
+  # Stop the database.
+  echo ""; echo ""
+  echo "${info}$0: Stopping database...${reset}"
+  echo "${info}----------------------------------------------------------------------${reset}"
+  if [ ${OS} == "linux" ]; then
+    sudo service postgresql stop
+    sudo update-rc.d postgresql disable
+  elif [ ${OS} == "osx" ]; then
+    brew services stop postgresql
+  fi
+
+  # Uninstall osm2pgsql.
+  echo ""; echo ""
+  echo "${inf}o$0: Uninstalling osm2pgsql database loading tool...${reset}"
+  echo "${info}----------------------------------------------------------------------${reset}"
+  if [ ${OS} == "linux" ]; then
+    sudo apt-get --assume-yes --purge remove osm2pgsql
+  elif [ ${OS} == "osx" ]; then
+    brew uninstall osm2pgsql
+  fi
+
+  # Uninstall postgis and dependencies.
+  echo ""; echo ""
+  echo "${info}$0: Uninstalling Postgis database extension...${reset}"
+  echo "${info}----------------------------------------------------------------------${reset}"
+  if [ ${OS} == "linux" ]; then
+    for s in $(dpkg -l | grep postgis | cut -d ' ' -f 3)
+    do
+      sudo apt-get --assume-yes --purge remove $s
+    done
+    sudo apt-get --assume-yes --purge remove libpng-dev
+  elif [ ${OS} == "osx" ]; then
+    brew uninstall postgis
+  fi
+    
+  # Uninstall Postgresql database.
+  echo ""; echo ""
+  echo "${info}$0: Uninstalling Postgres database...${reset}"
+  echo "${info}----------------------------------------------------------------------${reset}"
+  if [ ${OS} == "linux" ]; then
+    for s in $(dpkg -l | grep postgres | cut -d ' ' -f 3)
+    do
+      sudo apt-get --assume-yes --purge remove $s
+    done
+  elif [ ${OS} == "osx" ]; then
+    brew uninstall --ignore-dependencies postgresql
+    rm -rf /usr/local/var/postgres/data
+  fi
+
+  # Uninstall other unused packages.
+  echo ""; echo ""
+  echo "${inf}o$0: Uninstalling unused packages...${reset}"
+  echo "${info}----------------------------------------------------------------------${reset}"
+  if [ ${OS} == "linux" ]; then
+    sudo apt --assume-yes autoremove
+  fi
+
+  echo ""
+  echo "${info}Uninstall complete!${reset}";
 }
 
 # Ensure that options and arguments enterered are valid.
 if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
   print_intro; print_usage; exit 1
 fi
+if [ "$1" == "-u" ]; then
+  uninstall; exit 0
+fi
 if [ "$1" != "" ]; then
-  echo "${error}$0 Error: Invalid argument $1.${reset}"; print_usage; exit 1
+  echo "${error}Invalid argument $1.${reset}"; print_usage; exit 1
 fi
 
 echo "${success}$0: Starting...${reset}"
 echo "${success}----------------------------------------------------------------------${reset}"
-cd ${TILEMILL_DIR}
+
+# Ensure that installation tools are up to date.
+echo ""; echo ""
+echo "${info}$0: Updating installation tools...${reset}"
+echo "${info}----------------------------------------------------------------------${reset}"
+if [ ${OS} == "linux" ]; then
+  # Ensure that apt-get is up to date.
+  sudo apt-get update
+  if [ $? != 0 ]; then
+    echo "${error}Update of apt failed. Command:${reset} sudo apt-get update"; exit 1
+  fi
+elif [ ${OS} == "osx" ]; then
+  # Ensure that homebrew is up to date.
+  brew update
+  if [ $? != 0 ]; then
+    echo "${error}Update of apt failed. Command:${reset} brew update"; exit 1
+  fi
+fi
 
 # Install Postgresql database.
 echo ""; echo ""
 echo "${info}$0: Installing Postgres database...${reset}"
 echo "${info}----------------------------------------------------------------------${reset}"
-brew install postgresql
-if [ $? != 0 ]; then
-  echo "${error}$0 Error: Database installation failed.${reset} Command: brew install postgresql"; exit 1
+if [ ${OS} == "linux" ]; then
+  sudo apt-get --assume-yes install postgresql postgresql-contrib
+  if [ $? != 0 ]; then
+    echo "${error}Database installation failed.${reset} Command: sudo apt-get install postgresql postgresql-contrib"; exit 1
+  fi
+elif [ ${OS} == "osx" ]; then
+  brew install postgresql
+  if [ $? != 0 ]; then
+    echo "${error}Database installation failed.${reset} Command: brew install postgresql"; exit 1
+  fi
 fi
 
 # Install postgis and dependencies.
 echo ""; echo ""
 echo "${info}$0: Installing Postgis database extension...${reset}"
 echo "${info}----------------------------------------------------------------------${reset}"
-brew install libpng && brew link -f libpng
-if [ $? != 0 ]; then
-  echo "${error}$0 Error: libpng installation failed. Command:${reset} brew install libpng && brew link -f libpng"; exit 1
-fi
-brew install postgis
-if [ $? != 0 ]; then
-  echo "${error}$0 Error: Postgis extension installation failed. Command:${reset} brew install postgis"; exit 1
+if [ ${OS} == "linux" ]; then
+  sudo apt-get --assume-yes install libpng-dev postgis
+  if [ $? != 0 ]; then
+    echo "${error}libpng installation failed. Command:${reset} sudo apt-get install libpng-dev postgis"; exit 1
+  fi
+elif [ ${OS} == "osx" ]; then
+  brew install libpng && brew link -f libpng
+  if [ $? != 0 ]; then
+    echo "${error}libpng installation failed. Command:${reset} brew install libpng && brew link -f libpng"; exit 1
+  fi
+  brew install postgis
+  if [ $? != 0 ]; then
+    echo "${error}Postgis extension installation failed. Command:${reset} brew install postgis"; exit 1
+  fi
 fi
 
+# Prep and start database.
+echo ""; echo ""
+echo "${info}$0: Starting database...${reset}"
+echo "${info}----------------------------------------------------------------------${reset}"
+if [ ${OS} == "linux" ]; then
+  sudo update-rc.d postgresql enable
+  if [ $? != 0 ]; then
+    echo "${error}Database start-prep failed. Command:${reset} sudo update-rc.d postgresql enable"; exit 1
+  fi
+  sudo service postgresql initdb
+  if [ $? != 0 ]; then
+    echo "${error}Database init failed. Command:${reset} sudo service postgresql initdb"; exit 1
+  fi
+  sudo service postgresql start
+  if [ $? != 0 ]; then
+    echo "${error}Database start failed. Command:${reset} sudo service postgresql start"; exit 1
+  fi
+elif [ ${OS} == "osx" ]; then
+  if [ ! -d /usr/local/var/postgres/data ]; then
+    initdb /usr/local/var/postgres/data -E utf8
+    if [ $? != 0 ]; then
+      echo "${error}Database start-prep failed. Command:${reset} initdb /usr/local/var/postgres -E utf8"; exit 1
+    fi
+  fi
+  brew services start postgresql
+  if [ $? != 0 ]; then
+    echo "${error}Database start failed. Command:${reset} brew services start postgresql"; exit 1
+  fi
+fi
+sleep 5 # give postgres time to start
+    
 # Initialize database.
 echo ""; echo ""
 echo "${info}$0: Initializing database for OSM data...${reset}"
 echo "${info}----------------------------------------------------------------------${reset}"
-initdb /usr/local/var/postgres/data -E utf8
-if [ $? != 0 ]; then
-  echo "${error}$0 Error: Database initialization failed. Command:${reset} initdb /usr/local/var/postgres -E utf8"; exit 1
+if [ ${OS} == "linux" ]; then
+  # For linux, need to create a user account matching the logged-in user with necessary permissions.
+  # This has to be done from the default root DB user account: postgres.
+  sudo su -c "psql -U ${DB_USERDEF} -c \"create user ${DB_USERNAME};\"" postgres
+  sudo su -c "psql -U ${DB_USERDEF} -c \"ALTER USER ${DB_USERNAME} CREATEDB;\"" postgres
+  sudo su -c "psql -U ${DB_USERDEF} -c \"ALTER USER ${DB_USERNAME} WITH SUPERUSER;\"" postgres
 fi
-brew services start postgresql
-if [ $? != 0 ]; then
-  echo "${error}$0 Error: Database start failed. Command:${reset} brew services start postgresql"; exit 1
-fi
-sleep 10 # give postgres time to start
 # Just creating this because it is handy to be able to login to psql without specifying a DB and then to query the DBs.
-createdb -U ${DB_USERNAME} ${DB_USERNAME}
+createdb -U ${DB_USERNAME} ${DB_USERNAME} >& installdb.tmp
 if [ $? != 0 ]; then
-  echo "${error}$0 Error: Default database creation failed. Command:${reset} createdb -U ${DB_USERNAME} ${DB_USERNAME}"; exit 1
+  # If the error was that the DB already exists, then don't error.
+  grep "already exists" installdb.tmp >& /dev/null
+  if [ $? != 0 ]; then
+    echo "${error}Default database creation failed. Command:${reset} createdb -U ${DB_USERNAME} ${DB_USERNAME}"; exit 1
+  fi
 fi
-createdb -U ${DB_USERNAME} ${POSTGIS_DB}
+createdb -U ${DB_USERNAME} ${POSTGIS_DB} >& installdb.tmp
 if [ $? != 0 ]; then
-  echo "${error}$0 Error: PostGIS database creation failed. Command:${reset} createdb -U ${DB_USERNAME} ${POSTGIS_DB}"; exit 1
+  # If the error was that the DB already exists, then don't error.
+  grep "already exists" installdb.tmp >& /dev/null
+  if [ $? != 0 ]; then
+    echo "${error}PostGIS database creation failed. Command:${reset} createdb -U ${DB_USERNAME} ${POSTGIS_DB}"; rm installdb.tmp; exit 1
+  fi
 fi
-psql -U ${DB_USERNAME} -d ${POSTGIS_DB} -c "create extension postgis;"
+psql -U ${DB_USERNAME} -d ${POSTGIS_DB} -c "create extension postgis;" >& installdb.tmp
 if [ $? != 0 ]; then
-  echo "${error}$0 Error: PostGIS extension failed. Command:${reset} psql -U ${DB_USERNAME} -d ${POSTGIS_DB} -c "create extension postgis;""; exit 1
+  # If the error was that the DB already exists, then don't error.
+  grep "already exists" installdb.tmp >& /dev/null
+  if [ $? != 0 ]; then
+    echo "${error}PostGIS extension failed. Command:${reset} psql -U ${DB_USERNAME} -d ${POSTGIS_DB} -c \"create extension postgis;\""; rm installdb.tmp; exit 1
+  fi
 fi
-psql -U ${DB_USERNAME} -d ${POSTGIS_DB} -c "create extension hstore;"
+psql -U ${DB_USERNAME} -d ${POSTGIS_DB} -c "create extension hstore;" >& installdb.tmp
 if [ $? != 0 ]; then
-  echo "${error}$0 Error: Hstore extension failed. Command:${reset} psql -U ${DB_USERNAME} -d ${POSTGIS_DB} -c "create extension hstore;""; exit 1
+  # If the error was that the DB already exists, then don't error.
+  grep "already exists" installdb.tmp >& /dev/null
+  if [ $? != 0 ]; then
+    echo "${error}Hstore extension failed. Command:${reset} psql -U ${DB_USERNAME} -d ${POSTGIS_DB} -c \"create extension hstore;\""; rm installdb.tmp; exit 1
+  fi
 fi
-createdb -U ${DB_USERNAME} ${OSM_DB} -T ${POSTGIS_DB}
+createdb -U ${DB_USERNAME} ${OSM_DB} -T ${POSTGIS_DB} >& installdb.tmp
 if [ $? != 0 ]; then
-  echo "${error}$0 Error: OSM database creation failed. Command:${reset} createdb -U ${DB_USERNAME} ${OSM_DB} -T ${POSTGIS_DB}"; exit 1
+  # If the error was that the DB already exists, then don't error.
+  grep "already exists" installdb.tmp >& /dev/null
+  if [ $? != 0 ]; then
+    echo "${error}OSM database creation failed. Command:${reset} createdb -U ${DB_USERNAME} ${OSM_DB} -T ${POSTGIS_DB}"; rm installdb.tmp; exit 1
+  fi
 fi
+rm installdb.tmp
 
 # Install osm2pgsql.
 echo ""; echo ""
 echo "${info}$0: Installing osm2pgsql database loading tool...${reset}"
 echo "${info}----------------------------------------------------------------------${reset}"
-brew install osm2pgsql
-if [ $? != 0 ]; then
-  echo "${error}$0 Error: Database installation failed. Command:${reset} brew install osm2pgsql"; exit 1
+if [ ${OS} == "linux" ]; then
+  sudo apt-get --assume-yes install osm2pgsql
+  if [ $? != 0 ]; then
+    echo "${error}osm2pgsql installation failed. Command:${reset} sudo apt-get install osm2pgsql"; exit 1
+  fi
+elif [ ${OS} == "osx" ]; then
+  brew install osm2pgsql
+  if [ $? != 0 ]; then
+    echo "${error}osm2pgsql installation failed. Command:${reset} brew install osm2pgsql"; exit 1
+  fi
 fi
 
 echo ""; echo ""
--- utils/installtilemill.sh
+++ utils/installtilemill.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
+# Determine OS.
+info="$(tput setaf 6)";warning="$(tput setaf 3)Warning: ";error="$(tput setaf 1)Error: ";success="$(tput setaf 2)";reset=$(tput sgr0) # Colors for text
+case "${OSTYPE}" in
+  darwin*)
+    OS="osx";;
+  linux*)
+    OS="linux";;
+  solaris*)
+    echo "${warning}solaris OS detected, trying linux solution which may or may not work.${reset}";
+    OS="linux";;
+  bsd*)
+    echo "${warning}bsd OS detected, trying linux solution which may or may not work.${reset}";
+    OS="linux";;
+  msys*)
+    echo "${error}windows OS detected, to use this script on a windows machine, try running it in ubuntu on WSL.${reset}"; print_usage; exit 1;;
+  *)
+    echo "${warning}unknown OS detected, trying linux solution which may or may not work.${reset}";
+    OS="linux";;
+esac
+
 # Initialize global variables.
 NVM_VER="v0.34.0"                # Version of NVM that will be installed if no version is present.
 NODE_VER="lts/carbon"            # Version of Node that will be installed.
 VERSION=""                       # Variable used to hold the desired version.
-info=`tput setaf 6`;error=`tput setaf 1`;success=`tput setaf 2`;reset=`tput sgr0` # Colors for text
 # Set the versions that will be supported by this script (anything >= v1.0.0).
 for v in $(git tag -l | grep "^v" | grep -v "^v0")
 do
   VERSIONS+=( "$v" )
 done
-# The following vesions are added so that we can do testing.
-#VERSIONS+=( "v1.0.1-dev" )
+#VERSIONS+=( "v1.0.1-dev" ) # Version to use in testing.
 
 print_intro () {
   echo "This script will install or update TileMill. It is assumed that TileMill is already cloned using git and you are running this script from the tilemill/utils directory. This script only works for versions of TileMill > v1.0.0. If TileMill has not been installed before, this script will install Node and install the requested version of TileMill. If TileMill has been installed, this script will update Node and update TileMill to the requested version."
@@ -22,6 +40,7 @@ print_usage () {
   echo "    $0 version          Install the specified version of TileMill."
   echo "    $0 -h               Print help."
   echo "    $0 -v               List valid versions."
+  echo "    $0 -u               Uninstall."
 }
 
 # Print out a list of the valid TileMill versions that can be installed by this script.
@@ -45,26 +64,93 @@ validate_version () {
   return 1    # version not found in VERSION array.
 }
 
+# Touch the appropriate profile.
+touch_profile () {
+  if [ ${OS} == "osx" ]; then
+    touch ${HOME}/.bash_profile
+  else
+    touch ${HOME}/.bashrc
+  fi
+}
+
+# Source the appropriate profile.
+source_profile () {
+  if [ ${OS} == "osx" ]; then
+    source ${HOME}/.bash_profile
+  else
+    source ${HOME}/.bashrc
+  fi
+}
+
+# Uninstall a previous installation that was done using this script.
+uninstall () {
+  echo ""
+  echo "${warning}This function is only meant to uninstall components that were previously installed by this script. This will include uninstalling nodejs/npm, nvm, and resetting tilemill code to the default/master version. If you do not want any of these components uninstalled from your system, you should abort.${reset}"
+  echo "Continue with uninstall?[y/n]:";
+  read answer
+  if [ ${answer} != "y" ]; then
+      echo "${error}Aborting.${reset}"; exit 1
+  fi
+      
+  # Deactivate and uninstall node.
+  echo ""; echo ""
+  echo "${info}$0: Uninstalling Nodejs and npm...${reset}"
+  echo "${info}----------------------------------------------------------------------${reset}"
+  source ${HOME}/.nvm/nvm.sh
+  nvm deactivate
+  nvm uninstall ${NVM_VER}
+  nvm unload
+  unset NVM_DIR
+  
+  # Remove the nvm and npm cache/user files.
+  echo ""; echo ""
+  echo "${info}$0: Removing nvm and npm user/cache files...${reset}"
+  echo "${info}----------------------------------------------------------------------${reset}"
+  rm -rf ${HOME}/.nvm ${HOME}/.npm
+
+  # Update the appropriate profile to remove nvm.
+  echo ""; echo ""
+  echo "${info}$0: Removing nvm references from profile files...${reset}"
+  echo "${info}----------------------------------------------------------------------${reset}"
+  if [ ${OS} == "osx" ]; then
+    grep -iv nvm ${HOME}/.bash_profile > ${HOME}/.bash_profile.2
+    mv ${HOME}/.bash_profile.2 ${HOME}/.bash_profile
+  else
+    grep -iv nvm ${HOME}/.bashrc > ${HOME}/.bashrc.2
+    mv ${HOME}/.bashrc.2 ${HOME}/.bashrc
+  fi
+
+  # Resetting tilemill download to default/master code.
+  echo ""; echo ""
+  echo "${info}$0: Resetting tilemill download to default/master code...${reset}"
+  echo "${info}----------------------------------------------------------------------${reset}"
+  git checkout master
+
+  echo ""
+  echo "${info}Uninstall complete!${reset}";
+}
+
 # Ensure that options and arguments enterered are valid.
 if [ "$1" == "--help" ]; then
   print_intro; print_usage; exit 1
 fi
-while getopts ":hv" opt; do
+while getopts ":hvu" opt; do
   case ${opt} in
     h ) print_intro; print_usage; exit 1;;
     v ) print_versions; exit 1;;
-    \? ) echo "${error}Error: Invalid option.${reset}"; print_usage; exit 1;;
+    u ) uninstall; exit 0;;
+    \? ) echo "${error}Invalid option.${reset}"; print_usage; exit 1;;
   esac
 done
 if [ "$1" == "" ]; then
-  echo "${error}Error: version required.${reset}"; print_usage; exit 1
+  echo "${error}version required.${reset}"; print_usage; exit 1
 fi
 if [ "$2" != "" ]; then
-  echo "${error}Error: Invalid argument $2.${reset}"; print_usage; exit 1
+  echo "${error}Invalid argument $2.${reset}"; print_usage; exit 1
 fi
 validate_version "$1"
 if [ $? != 0 ]; then
-  echo "${error}Error: Invalid version $1.${reset}"; print_usage; exit 1
+  echo "${error}Invalid version $1.${reset}"; print_usage; exit 1
 fi
 VERSION="$1"
 
@@ -77,18 +163,34 @@ echo ""; echo ""
 echo "${info}$0: Installing Node Version Manager if not already there...${reset}"
 echo "${info}----------------------------------------------------------------------${reset}"
 if [ ! -e ${HOME}/.nvm/nvm.sh ]; then
+
+  # Installing NVM pre-requisites if in Linux OS.
+  if [ ${OS} == "linux" ]; then
+    # Ensure that apt-get is up to date.
+    sudo apt-get update
+    if [ $? != 0 ]; then
+      echo "${error}Update of apt failed. Command:${reset} sudo apt-get update"; exit 1
+    fi
+    # Install packages that may be needed by nvm.
+    sudo apt-get --assume-yes install build-essential libssl-dev
+    if [ $? != 0 ]; then
+      echo "${error}Installation of pre-requisite packages failed. Command:${reset} sudo apt-get install build-essential libssl-dev"; exit 1
+    fi
+  fi
+
   # Installing NVM.
-  touch ~/.bash_profile
+  touch_profile
+  unset NVM_DIR
   # From instructions found at https://github.com/creationix/nvm
   curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VER}/install.sh | bash
   if [ $? != 0 ]; then
-    echo "${error}Error: Installation of Node Version Manager failed. Command:${reset} curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VER}/install.sh | bash"; exit 1
+    echo "${error}Installation of Node Version Manager failed. Command:${reset} curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VER}/install.sh | bash"; exit 1
   fi
-  source ~/.bash_profile
+  source_profile
 
   # Checking to see if nvm is now there after the install.
   if [ ! -e ${HOME}/.nvm/nvm.sh ]; then
-    echo "${error}Error: Installation of Node Version Manager failed.${reset}"; exit 1
+    echo "${error}Installation of Node Version Manager failed.${reset}"; exit 1
   fi
 fi
 source ${HOME}/.nvm/nvm.sh
@@ -99,13 +201,13 @@ echo "${info}$0: Installing/updating Node...${reset}"
 echo "${info}----------------------------------------------------------------------${reset}"
 nvm install ${NODE_VER}
 if [ $? != 0 ]; then
-  echo "${error}Error: Installation of Node failed. Command:${reset} nvm install ${NODE_VER}"; exit 1
+  echo "${error}Installation of Node failed. Command:${reset} nvm install ${NODE_VER}"; exit 1
 fi
 nvm install-latest-npm
 if [ $? != 0 ]; then
-  echo "${error}Error: Updating npm to the latest version failed. Command:${reset} nvm install-latest-npm"; exit 1
+  echo "${error}Updating npm to the latest version failed. Command:${reset} nvm install-latest-npm"; exit 1
 fi
-source ~/.bash_profile
+source_profile
 
 # Update TileMill source to the correct version.
 echo ""; echo ""
@@ -113,15 +215,15 @@ echo "${info}$0: Updating TileMill source to the correct version...${reset}"
 echo "${info}----------------------------------------------------------------------${reset}"
 git fetch --tags
 if [ $? != 0 ]; then
-  echo "${error}Error: Fetch of tags failed. Command:${reset} git fetch --tags"; exit 1
+  echo "${error}Fetch of tags failed. Command:${reset} git fetch --tags"; exit 1
 fi
 git fetch
 if [ $? != 0 ]; then
-  echo "${error}Error: Fetch of tags failed. Command:${reset} git fetch"; exit 1
+  echo "${error}Fetch of tags failed. Command:${reset} git fetch"; exit 1
 fi
 git checkout $VERSION
 if [ $? != 0 ]; then
-  echo "${error}Error: Checkout of version $VERSION failed. Command:${reset} git checkout $VERSION"; exit 1
+  echo "${error}Checkout of version $VERSION failed. Command:${reset} git checkout $VERSION"; exit 1
 fi
 
 # Install TileMill from source.
@@ -130,7 +232,7 @@ echo "${info}$0: Installing TileMill from source...${reset}"
 echo "${info}----------------------------------------------------------------------${reset}"
 npm clean-install
 if [ $? != 0 ]; then
-  echo "${error}Error: Install of TileMill failed. Command:${reset} npm clean-install"; exit 1
+  echo "${error}Install of TileMill failed. Command:${reset} npm clean-install"; exit 1
 fi
 
 echo ""; echo ""
